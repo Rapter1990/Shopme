c9967ad36bd35fcaeb2e689682a2c0227f34bede
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-08 13:22:56 +0300

    Task 19 : Add EntityScan in ShopmeBackEndApplication

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
@@ -2,8 +2,10 @@ package com.shopme.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"com.shopme.common.entity", "com.shopme.admin.user"})
 public class ShopmeBackEndApplication {
 
 	public static void main(String[] args) {
