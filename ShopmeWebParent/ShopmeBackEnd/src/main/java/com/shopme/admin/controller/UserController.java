2b51565a4cd48d9a1adf37534f9e0d808214fccd
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-10 13:31:52 +0300

    Task 29 : Add function for User Form in User Service and User Controller

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/UserController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.service.UserService;
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Controller
@@ -22,5 +25,29 @@ public class UserController {
 		model.addAttribute("listUsers", listUsers);
 		
 		return "users";
+	}
+	
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		List<Role> listRoles = service.listRoles();
+		
+		User user = new User();
+		user.setEnabled(true);
+		
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+		model.addAttribute("pageTitle", "Create New User");
+		
+		return "user_form";
+	}
+	
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		service.save(user);
+		
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+		
+		return "redirect:/users";
 	}
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/UserService.java
@@ -7,20 +7,46 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.shopme.admin.repository.RoleRepository;
 import com.shopme.admin.repository.UserRepository;
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Service
 @Transactional
 public class UserService {
 
-	@Autowired
 	private UserRepository userRepo;
+	private RoleRepository roleRepo;
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public UserService(UserRepository userRepo, RoleRepository roleRepo, PasswordEncoder passwordEncoder) {
+		super();
+		this.userRepo = userRepo;
+		this.roleRepo = roleRepo;
+		this.passwordEncoder = passwordEncoder;
+	}
 	
 	public List<User> listAll() {
 		List<User> userList = new ArrayList<>();
 		userRepo.findAll().forEach(userList::add);
 		return userList;
+	}
+	
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepo.findAll();
+	}
+	
+	public void save(User user) {
+		encodePassword(user);
+		userRepo.save(user);
+	}
+	
+	private void encodePassword(User user) {
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
 	}
 }
