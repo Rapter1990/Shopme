0425298cc867dbca09ba700b7479faab5e93da67
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-10 13:32:23 +0300

    Task 31 : Add PasswordEncoderTest

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/PasswordEncoderTest.java
@@ -0,0 +1,22 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderTest {
+
+	@Test
+	public void testEncodePassword() {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String rawPassword = "ab2020";
+		String encodedPassword = passwordEncoder.encode(rawPassword);
+		
+		System.out.println(encodedPassword);
+		
+		boolean matches = passwordEncoder.matches(rawPassword, encodedPassword);
+		
+		assertThat(matches).isTrue();
+	}
+}
