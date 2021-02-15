8fc5d409287e006b35f844572c2edd5669c743cd
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-16 00:10:03 +0300

    Task 47 : Define editUser in UserController

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/UserController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.admin.error.UserNotFoundException;
 import com.shopme.admin.service.UserService;
 import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
@@ -46,8 +48,27 @@ public class UserController {
 		System.out.println(user);
 		service.save(user);
 		
-		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+		redirectAttributes.addFlashAttribute("messageSuccess", "The user has been saved successfully.");
 		
 		return "redirect:/users";
+	}
+	
+	@GetMapping("/users/edit/{id}")
+	public String editUser(@PathVariable(name = "id") Integer id, 
+			Model model,
+			RedirectAttributes redirectAttributes) {
+		try {
+			User user = service.get(id);
+			List<Role> listRoles = service.listRoles();
+			
+			model.addAttribute("user", user);
+			model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
+			model.addAttribute("listRoles", listRoles);
+			
+			return "user_form";
+		} catch (UserNotFoundException ex) {
+			redirectAttributes.addFlashAttribute("messageError", ex.getMessage());
+			return "redirect:/users";
+		}
 	}
 }
