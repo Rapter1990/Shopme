239ee807e38745d7ea396c82a08978d49abfea25
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-27 13:48:48 +0300

    Task 66 : Changed listAll to ListFirstPage and Add listByPage in UserController

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/UserController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -27,11 +28,36 @@ public class UserController {
 	private UserService service;
 	
 	@GetMapping("/users")
-	public String listAll(Model model) {
-		List<User> listUsers = service.listAll();
-		model.addAttribute("listUsers", listUsers);
+	public String listFirstPage(Model model) {
+		return listByPage(1, model);
+	}
+	
+	@GetMapping("/users/page/{pageNum}")
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
+		Page<User> page = service.listByPage(pageNum);
+		List<User> listUsers = page.getContent();
+
+		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
+		long endCount = startCount + UserService.USERS_PER_PAGE - 1;
 		
-		return "users";
+		System.out.println("startCount : " + startCount);
+		System.out.println("endCount : " + endCount);
+		
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+		
+		System.out.println("page.getTotalElements() : " + page.getTotalElements());
+		System.out.println("endCount (endCount > page.getTotalElements()): " + endCount);
+
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("listUsers", listUsers);
+
+		return "users";	
 	}
 	
 	@GetMapping("/users/new")
