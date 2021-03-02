286cf606151a23e662389f601fc043bf09e2323d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-02 11:24:44 +0300

    Task 68 : Define sorting process in listByPage method of UserService

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/UserService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -109,8 +110,13 @@ public class UserService {
 		userRepo.updateEnabledStatus(id, enabled);
 	}
 	
-	public Page<User> listByPage(int pageNum) {
-		Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE);
+	public Page<User> listByPage(int pageNum, String sortField, String sortDir) {
+		Sort sort = Sort.by(sortField);
+
+		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
+
+		Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE, sort);
+		
 		return userRepo.findAll(pageable);
 	}
 }
