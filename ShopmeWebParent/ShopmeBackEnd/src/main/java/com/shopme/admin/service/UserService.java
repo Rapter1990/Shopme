a3e7578ce927d9361abdcccbce0d715fe384020a
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-03 12:00:14 +0300

    Task 73 : Add seach keyword in listByPage method of UserService

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/UserService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/UserService.java
@@ -110,12 +110,16 @@ public class UserService {
 		userRepo.updateEnabledStatus(id, enabled);
 	}
 	
-	public Page<User> listByPage(int pageNum, String sortField, String sortDir) {
+	public Page<User> listByPage(int pageNum, String sortField, String sortDir, String keyword) {
 		Sort sort = Sort.by(sortField);
 
 		sort = sortDir.equals("asc") ? sort.ascending() : sort.descending();
 
 		Pageable pageable = PageRequest.of(pageNum - 1, USERS_PER_PAGE, sort);
+		
+		if (keyword != null) {
+			return userRepo.findAll(keyword, pageable);
+		}
 		
 		return userRepo.findAll(pageable);
 	}
