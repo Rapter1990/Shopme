1f07dbcb0f2c15ad57c905d950a26efe3a747452
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-03 11:59:21 +0300

    Task 72 : Add testSearchUsers in UserRepositoryTests

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -145,4 +145,21 @@ public class UserRepositoryTests {
 
 		assertThat(listUsers.size()).isEqualTo(pageSize);
 	}
+	
+	@Test
+	public void testSearchUsers() {
+		String keyword = "Remzi";
+
+		int pageNumber = 0;
+		int pageSize = 4;
+
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		Page<User> page = repo.findAll(keyword, pageable);
+
+		List<User> listUsers = page.getContent();
+
+		listUsers.forEach(user -> System.out.println(user));	
+
+		assertThat(listUsers.size()).isGreaterThan(0);
+	}
 }
