8b749acc8901d12c60a09b0b4c418f5dc0942895
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-05 11:00:27 +0300

    Task 78 : Create DirectUtil and Define getReirectURLtoAfftectedUser

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/DirectUtil.java
@@ -0,0 +1,11 @@
+package com.shopme.admin.util;
+
+import com.shopme.common.entity.User;
+
+public class DirectUtil {
+
+	public static String getRedirectURLtoAffectedUser(User user) {
+		String firstPartOfEmail = user.getEmail().split("@")[0];
+		return "redirect:/users/page/1?sortField=id&sortDir=asc&keyword=" + firstPartOfEmail;
+	}
+}
