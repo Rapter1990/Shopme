0e0aa7d77961bb7e5011c201152a0e38f06ac8d9
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-07 15:25:29 +0300

    Task 81 : Create AbstractExporter to define file information

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/AbstractExporter.java
@@ -0,0 +1,25 @@
+package com.shopme.admin.util;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class AbstractExporter {
+
+	public void setResponseHeader(HttpServletResponse response, String contentType, 
+			String extension) throws IOException {
+		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+		String timestamp = dateFormatter.format(new Date());
+		String fileName = "users_" + timestamp + extension;
+
+		response.setContentType(contentType);
+
+		String headerKey = "Content-Disposition";
+		String headerValue = "attachment; filename=" + fileName;
+		response.setHeader(headerKey, headerValue);
+
+	}	
+}
