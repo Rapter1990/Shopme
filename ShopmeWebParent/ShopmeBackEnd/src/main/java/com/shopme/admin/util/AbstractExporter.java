21c1e7b3f8c96cc4075ebb352a42cd6c146197c7
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-08 01:18:03 +0300

    Task 87 : Add utf-8 feature in AbstractExporter class

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/AbstractExporter.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/AbstractExporter.java
@@ -14,8 +14,10 @@ public class AbstractExporter {
 		DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
 		String timestamp = dateFormatter.format(new Date());
 		String fileName = "users_" + timestamp + extension;
+		String utf = "UTF-8";
 
 		response.setContentType(contentType);
+		response.setCharacterEncoding(utf);
 
 		String headerKey = "Content-Disposition";
 		String headerValue = "attachment; filename=" + fileName;
