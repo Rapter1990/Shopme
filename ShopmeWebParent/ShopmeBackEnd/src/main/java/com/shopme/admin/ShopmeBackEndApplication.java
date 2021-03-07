e312853090caa14edf2967c488c5483d8132d51c
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-08 01:35:33 +0300

    Task 91 : Remove unnecessary import from ShopingBackEndApplication

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/ShopmeBackEndApplication.java
@@ -3,7 +3,6 @@ package com.shopme.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan({"com.shopme.common.entity"})
