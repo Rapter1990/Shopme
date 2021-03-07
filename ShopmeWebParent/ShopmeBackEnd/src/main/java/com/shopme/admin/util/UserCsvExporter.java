f798b99a0a3f4ebd11b0ccbff2db9ca5b3338314
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-07 15:26:26 +0300

    Task 82 : Remove file information code and access it via AbstractExporter

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/UserCsvExporter.java
@@ -0,0 +1,37 @@
+package com.shopme.admin.util;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import com.shopme.common.entity.User;
+
+public class UserCsvExporter extends AbstractExporter{
+
+	public void export(List<User> listUsers, HttpServletResponse response) throws IOException {
+		
+		super.setResponseHeader(response, "text/csv", ".csv");
+
+		ICsvBeanWriter csvWriter = new CsvBeanWriter(response.getWriter(), 
+				CsvPreference.STANDARD_PREFERENCE);
+
+		String[] csvHeader = {"User ID", "E-mail", "First Name", "Last Name", "Roles", "Enabled"};
+		String[] fieldMapping = {"id", "email", "firstName", "lastName", "roles", "enabled"};
+
+		csvWriter.writeHeader(csvHeader);
+
+		for (User user : listUsers) {
+			csvWriter.write(user, fieldMapping);
+		}
+
+		csvWriter.close();
+	}
+}
