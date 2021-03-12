07ca77e8b732cd2d8cda878c0a8438fd32e10ec0
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-12 11:48:37 +0300

    Task 102 : Add login configuration in WebSecurityConfig

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ package com.shopme.admin.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,7 +22,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests()
+		.anyRequest().authenticated()
+		.and()
+		.formLogin()			
+			.loginPage("/login")
+			.usernameParameter("email")
+			.permitAll();
+	}
+	
+	// Before authenticated, all matchers can be ignored and all these are performed.
+	// Like -> showing image in /login , all images,js and web jars files are detected  
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/images/**", "/js/**", "/webjars/**");
 	}
 	
 }
