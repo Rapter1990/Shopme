977e470c191f74cb763b24811cd2ccd18604b844
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-17 11:58:40 +0300

    Task 107 : Add FetchType.EAGER in User

--- ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -54,7 +55,7 @@ public class User {
 		this.lastName = lastName;
 	}
 	
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(
 			name = "users_roles",
 			joinColumns = @JoinColumn(name = "user_id"),
