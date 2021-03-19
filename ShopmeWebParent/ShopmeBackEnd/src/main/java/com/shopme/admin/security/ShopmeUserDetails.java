9567bcd2b8d5957741fbfd6ed282ddc25888e0eb
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-19 11:51:00 +0300

    Task 114 : Define setFirstName and setLastName method in ShopmeUserDetails

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
@@ -73,5 +73,13 @@ public class ShopmeUserDetails implements UserDetails {
 	public String getFullname() {
 		return this.user.getFirstName() + " " + this.user.getLastName();
 	}
+	
+	public void setFirstName(String firstName) {
+		this.user.setFirstName(firstName);
+	}
+
+	public void setLastName(String lastName) {
+		this.user.setLastName(lastName);
+	}	
 
 }
