af9a70a040421bdbc19c9776f54f06326c15883d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-22 10:54:54 +0300

    Task 118 : Create common_form.js to remove duplicate codes from account_form,user_form and users.html

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+$(document).ready(function() {
+	$("#buttonCancel").on("click", function() {
+		window.location = moduleURL;
+	});
+
+	$("#fileImage").change(function() {
+		fileSize = this.files[0].size;
+
+		if (fileSize > 1048576) {
+			this.setCustomValidity("You must choose an image less than 1MB!");
+			this.reportValidity();
+		} else {
+			this.setCustomValidity("");
+			showImageThumbnail(this);				
+		}
+
+	});
+});
+
+function showImageThumbnail(fileInput) {
+	var file = fileInput.files[0];
+	var reader = new FileReader();
+	reader.onload = function(e) {
+		$("#thumbnail").attr("src", e.target.result);
+	};
+
+	reader.readAsDataURL(file);
+} 
