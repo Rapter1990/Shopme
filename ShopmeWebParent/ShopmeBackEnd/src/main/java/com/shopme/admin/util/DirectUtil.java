2aad8c4065b215d5f4e25905543c1eb4fb2523a8
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-22 13:39:54 +0300

    Task 125 : Redefine firstPartOfEmail in getDirectURLtoAffectedUser of DirectUtil

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/DirectUtil.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/DirectUtil.java
@@ -5,7 +5,8 @@ import com.shopme.common.entity.User;
 public class DirectUtil {
 
 	public static String getRedirectURLtoAffectedUser(User user) {
-		String firstPartOfEmail = user.getEmail().split("@")[0];
+		//String firstPartOfEmail = user.getEmail().split("@")[0];
+		String firstPartOfEmail = user.getEmail();
 		return "redirect:/users/page/1?sortField=id&sortDir=asc&keyword=" + firstPartOfEmail;
 	}
 }
