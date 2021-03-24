7bb71a29d57ba4de4ace43ac2ce8b48b9d810b89
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-24 11:15:59 +0300

    Task 139 : Create Category and CategoryRepository

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -0,0 +1,49 @@
+package com.shopme.common.entity;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "categories")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Category implements Serializable{
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(length = 128, nullable = false, unique = true)
+	private String name;
+
+	@Column(length = 64, nullable = false, unique = true)
+	private String alias;
+
+	@Column(length = 128, nullable = false)
+	private String image;
+
+	private boolean enabled;
+	
+	@OneToOne
+	@JoinColumn(name = "parent_id")
+	private Category parent;
+
+	@OneToMany(mappedBy = "parent")
+	private Set<Category> children = new HashSet<>();
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.admin.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.shopme.common.entity.Category;
+
+public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer>{
+
+}
