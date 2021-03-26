5431e8996e834feaa7580cf3a59ba8b1d74dc03d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-26 12:32:35 +0300

    Task 141 : Create CategoryRepository

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/CategoryRepositoryTests.java
@@ -0,0 +1,101 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.admin.repository.CategoryRepository;
+import com.shopme.common.entity.Category;
+
+@DataJpaTest(showSql = false)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class CategoryRepositoryTests {
+
+	@Autowired
+	private CategoryRepository repo;
+	
+	@Test
+	public void testCreateRootCategory() {
+		Category category = new Category("Electronics");
+		Category savedCategory = repo.save(category);
+
+		assertThat(savedCategory.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testCreateSubCategory() {
+		
+		Category parent = new Category(7);
+		Category subCategory = new Category("iPhone", parent);
+		Category savedCategory = repo.save(subCategory);
+
+		assertThat(savedCategory.getId()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testCreateMultiSubCategory() {
+		
+		Category parent = new Category(2);
+		Category subCategory1 = new Category("Cameras", parent);
+		Category subCategory2 = new Category("Smartphones", parent);
+
+		repo.saveAll(List.of(subCategory1, subCategory2));
+
+	}
+	
+	@Test
+	public void testGetCategory() {
+		Category category = repo.findById(1).get();
+		System.out.println(category.getName());
+
+		Set<Category> children = category.getChildren();
+
+		for (Category subCategory : children) {
+			System.out.println(subCategory.getName());	
+		}
+
+		assertThat(children.size()).isGreaterThan(0);
+	}
+	
+	@Test
+	public void testPrintHierarchicalCategories() {
+		Iterable<Category> categories = repo.findAll();
+
+		for (Category category : categories) {
+			if (category.getParent() == null) {
+				System.out.println(category.getName());
+
+				Set<Category> children = category.getChildren();
+
+				for (Category subCategory : children) {
+					System.out.println("--" + subCategory.getName());
+					printChildren(subCategory, 1);
+				}
+			}
+		}
+	}
+	
+	private void printChildren(Category parent, int subLevel) {
+		int newSubLevel = subLevel + 1;
+		Set<Category> children = parent.getChildren();
+
+		for (Category subCategory : children) {
+			for (int i = 0; i < newSubLevel; i++) {				
+				System.out.print("--");
+			}
+
+			System.out.println(subCategory.getName());
+
+			printChildren(subCategory, newSubLevel);
+		}		
+	}
+}
