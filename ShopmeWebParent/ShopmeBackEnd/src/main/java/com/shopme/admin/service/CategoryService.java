55a6aaca14cd0331abf7677f9c5177a6a93dc033
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-28 20:58:18 +0300

    Task 148 : Add listCategoriesUsedInForm in CategoryService

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/CategoryService.java
@@ -2,6 +2,7 @@ package com.shopme.admin.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.transaction.Transactional;
 
@@ -25,6 +26,48 @@ public class CategoryService implements ICategoryService{
 		List<Category> categoryList = new ArrayList<>();
 		categoryRepository.findAll().forEach(categoryList::add);
 		return categoryList;
+	}
+
+	@Override
+	public List<Category> listCategoriesUsedInForm() {
+		
+		List<Category> categoriesUsedInForm = new ArrayList<>();
+
+		Iterable<Category> categoriesInDB = categoryRepository.findAll();
+
+		for (Category category : categoriesInDB) {
+			if (category.getParent() == null) {
+				categoriesUsedInForm.add(new Category(category.getName()));
+
+				Set<Category> children = category.getChildren();
+
+				for (Category subCategory : children) {
+					String name = "--" + subCategory.getName();
+					categoriesUsedInForm.add(new Category(name));
+
+					listChildren(categoriesUsedInForm, subCategory, 1);
+				}
+			}
+		}		
+
+		return categoriesUsedInForm;
+	}
+	
+	private void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+		int newSubLevel = subLevel + 1;
+		Set<Category> children = parent.getChildren();
+
+		for (Category subCategory : children) {
+			String name = "";
+			for (int i = 0; i < newSubLevel; i++) {				
+				name += "--";
+			}
+			name += subCategory.getName();
+
+			categoriesUsedInForm.add(new Category(name));
+
+			listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+		}		
 	}
 
 }
