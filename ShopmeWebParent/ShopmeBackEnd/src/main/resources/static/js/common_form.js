0b71d46e1f1d871586a66d34cde983ee54f7d085
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-31 11:48:33 +0300

    Task 155 : Change the size of uploading file in common_js

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
 	$("#fileImage").change(function() {
 		fileSize = this.files[0].size;
 
-		if (fileSize > 1048576) {
-			this.setCustomValidity("You must choose an image less than 1MB!");
+		if (fileSize > 102400) {
+			this.setCustomValidity("You must choose an image less than 100KB!");
 			this.reportValidity();
 		} else {
 			this.setCustomValidity("");
