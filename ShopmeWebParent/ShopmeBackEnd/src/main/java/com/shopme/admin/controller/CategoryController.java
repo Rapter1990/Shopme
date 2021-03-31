bc44b9494c3112aa413a5bfa4669d35fa1124fb8
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-03-31 11:47:50 +0300

    Task 154 : Add saveCategory in CategoryController

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/CategoryController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/CategoryController.java
@@ -10,9 +10,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.service.CategoryService;
+import com.shopme.admin.util.FileUploadUtil;
 import com.shopme.common.entity.Category;
 
 @Controller
@@ -51,6 +57,21 @@ public class CategoryController {
 
 		return "categories/category_form";
 		
+	}
+	
+	@PostMapping("/categories/save")
+	public String saveCategory(Category category, 
+			@RequestParam("fileImage") MultipartFile multipartFile,
+			RedirectAttributes ra) throws IOException {
+		String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+		category.setImage(fileName);
+
+		Category savedCategory = categoryService.save(category);
+		String uploadDir = "../category-images/" + savedCategory.getId();
+		FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+
+		ra.addFlashAttribute("messageSuccess", "The category has been saved successfully.");
+		return "redirect:/categories";
 	}
 	
 	@GetMapping("/categories/export/csv")
