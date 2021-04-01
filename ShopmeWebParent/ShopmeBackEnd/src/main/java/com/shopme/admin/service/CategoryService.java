3c9a0bebc93fff6b4e5359c9555e84d190d10483
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-01 11:27:01 +0300

    Task 159 : Revise listAll method and change listSubCategoriesUsedInForm from listChildren in CategoryService

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/CategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/CategoryService.java
@@ -23,9 +23,46 @@ public class CategoryService implements ICategoryService{
 	@Override
 	public List<Category> listAll() {
 		// TODO Auto-generated method stub
-		List<Category> categoryList = new ArrayList<>();
-		categoryRepository.findAll().forEach(categoryList::add);
-		return categoryList;
+		List<Category> rootCategories = categoryRepository.findRootCategories();
+		return listHierarchicalCategories(rootCategories);
+	}
+	
+	private List<Category> listHierarchicalCategories(List<Category> rootCategories) {
+		List<Category> hierarchicalCategories = new ArrayList<>();
+
+		for (Category rootCategory : rootCategories) {
+			hierarchicalCategories.add(Category.copyFull(rootCategory));
+
+			Set<Category> children = rootCategory.getChildren();
+
+			for (Category subCategory : children) {
+				String name = "--" + subCategory.getName();
+				hierarchicalCategories.add(Category.copyFull(subCategory, name));
+
+				listSubHierarchicalCategories(hierarchicalCategories, subCategory, 1);
+			}
+		}
+
+		return hierarchicalCategories;
+	}
+
+	private void listSubHierarchicalCategories(List<Category> hierarchicalCategories,
+			Category parent, int subLevel) {
+		Set<Category> children = parent.getChildren();
+		int newSubLevel = subLevel + 1;
+
+		for (Category subCategory : children) {
+			String name = "";
+			for (int i = 0; i < newSubLevel; i++) {				
+				name += "--";
+			}
+			name += subCategory.getName();
+
+			hierarchicalCategories.add(Category.copyFull(subCategory, name));
+
+			listSubHierarchicalCategories(hierarchicalCategories, subCategory, newSubLevel);
+		}
+
 	}
 
 	@Override
@@ -45,7 +82,7 @@ public class CategoryService implements ICategoryService{
 					String name = "--" + subCategory.getName();
 					categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
 
-					listChildren(categoriesUsedInForm, subCategory, 1);
+					listSubCategoriesUsedInForm(categoriesUsedInForm, subCategory, 1);
 				}
 			}
 		}		
@@ -53,7 +90,8 @@ public class CategoryService implements ICategoryService{
 		return categoriesUsedInForm;
 	}
 	
-	private void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+	private void listSubCategoriesUsedInForm(List<Category> categoriesUsedInForm, 
+			Category parent, int subLevel){
 		int newSubLevel = subLevel + 1;
 		Set<Category> children = parent.getChildren();
 
@@ -66,7 +104,7 @@ public class CategoryService implements ICategoryService{
 
 			categoriesUsedInForm.add(Category.copyIdAndName(subCategory.getId(), name));
 
-			listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+			listSubCategoriesUsedInForm(categoriesUsedInForm, subCategory, newSubLevel);
 		}		
 	}
 
