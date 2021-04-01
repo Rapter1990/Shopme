f8d946140600ba09a3a59f380fb751da39d7e00b
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-01 11:28:51 +0300

    Task 161 : Add testListRootCategories in CategoryRepositoryTests

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/CategoryRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/CategoryRepositoryTests.java
@@ -98,4 +98,10 @@ public class CategoryRepositoryTests {
 			printChildren(subCategory, newSubLevel);
 		}		
 	}
+	
+	@Test
+	public void testListRootCategories() {
+		List<Category> rootCategories = repo.findRootCategories();
+		rootCategories.forEach(cat -> System.out.println(cat.getName()));
+	}
 }
