01c40df7a9148e5cbad09b777ed1f062ba36cf04
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-01 11:25:19 +0300

    Task 158 : Add findRootCategories in CategoryRepository

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/CategoryRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/CategoryRepository.java
@@ -1,9 +1,14 @@
 package com.shopme.admin.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.shopme.common.entity.Category;
 
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer>{
 
+	@Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
+	public List<Category> findRootCategories();
 }
