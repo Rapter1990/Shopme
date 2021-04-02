7000f8840826b157bec543bfd2cb5e175957688d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-02 11:39:49 +0300

    Task 167 : Define get method in ICategoryService

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/ICategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/ICategoryService.java
@@ -2,6 +2,7 @@ package com.shopme.admin.service.impl;
 
 import java.util.List;
 
+import com.shopme.admin.error.CategoryNotFoundException;
 import com.shopme.common.entity.Category;
 
 public interface ICategoryService {
@@ -11,4 +12,6 @@ public interface ICategoryService {
 	public List<Category> listCategoriesUsedInForm();
 	
 	public Category save(Category category);
+	
+	public Category get(Integer id) throws CategoryNotFoundException;
 }
