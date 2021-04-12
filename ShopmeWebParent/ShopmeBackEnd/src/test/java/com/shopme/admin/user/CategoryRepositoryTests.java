507f2ec8c981a8371206448b02879148015e6ca6
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-12 12:57:57 +0300

    Task 175 : Add testFindByName and testFindByAlias in CategoryRepositoryTests

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/CategoryRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/CategoryRepositoryTests.java
@@ -104,4 +104,22 @@ public class CategoryRepositoryTests {
 		List<Category> rootCategories = repo.findRootCategories();
 		rootCategories.forEach(cat -> System.out.println(cat.getName()));
 	}
+	
+	@Test
+	public void testFindByName() {
+		String name = "Computers"; // Computers1
+		Category category = repo.findByName(name);
+
+		assertThat(category).isNotNull();
+		assertThat(category.getName()).isEqualTo(name);
+	}
+	
+	@Test
+	public void testFindByAlias() {
+		String alias = "Electronics";
+		Category category = repo.findByAlias(alias);
+
+		assertThat(category).isNotNull();
+		assertThat(category.getAlias()).isEqualTo(alias);
+	}
 }
