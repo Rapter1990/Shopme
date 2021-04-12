55f3788b6ca8ab0726cbcfefdf2c8a58cee16da7
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-12 13:11:32 +0300

    Task 181 : Define new methos for modal dialog in common.js

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common.js
@@ -23,3 +23,18 @@ function customizeDropDownMenu() {
 		location.href = this.href;
 	});
 } 
+
+
+function showModalDialog(title, message) {
+	$("#modalTitle").text(title);
+	$("#modalBody").text(message);
+	$("#modalDialog").modal();
+}
+
+function showErrorModal(message) {
+	showModalDialog("Error", message);
+}
+
+function showWarningModal(message) {
+	showModalDialog("Warning", message);
+}
