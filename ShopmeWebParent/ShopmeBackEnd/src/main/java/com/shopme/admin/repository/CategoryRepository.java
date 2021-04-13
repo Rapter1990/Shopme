3e996f9072fc2afbe7f5e3c2faf749d935994047
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-13 12:46:35 +0300

    Task 186 : Define sort property in findRootCategories of CategoryRepository

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/CategoryRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/CategoryRepository.java
@@ -2,6 +2,7 @@ package com.shopme.admin.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -10,7 +11,7 @@ import com.shopme.common.entity.Category;
 public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer>{
 
 	@Query("SELECT c FROM Category c WHERE c.parent.id is NULL")
-	public List<Category> findRootCategories();
+	public List<Category> findRootCategories(Sort sort);
 	
 	public Category findByName(String name);
 
