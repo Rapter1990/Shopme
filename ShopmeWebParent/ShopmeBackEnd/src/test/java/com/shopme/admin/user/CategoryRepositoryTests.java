00523435be09a81734f6b04975a46004aafe22e4
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-13 12:48:05 +0300

    Task 188 : Define sort process in testListRootCategories of CategoryRepositoryTests

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/CategoryRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/CategoryRepositoryTests.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 
 import com.shopme.admin.repository.CategoryRepository;
@@ -101,7 +102,7 @@ public class CategoryRepositoryTests {
 	
 	@Test
 	public void testListRootCategories() {
-		List<Category> rootCategories = repo.findRootCategories();
+		List<Category> rootCategories = repo.findRootCategories(Sort.by("name").ascending());
 		rootCategories.forEach(cat -> System.out.println(cat.getName()));
 	}
 	
