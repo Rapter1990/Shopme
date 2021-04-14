c6a8152c62b46d0ae5f8c68d68b857baa8766456
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-14 11:56:26 +0300

    Task 193 : Define updateCategoryEnabledStatus in CategoryRepository

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/CategoryRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/CategoryRepository.java
@@ -3,6 +3,7 @@ package com.shopme.admin.repository;
 import java.util.List;
 
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
@@ -16,4 +17,8 @@ public interface CategoryRepository extends PagingAndSortingRepository<Category,
 	public Category findByName(String name);
 
 	public Category findByAlias(String alias);
+	
+	@Query("UPDATE Category c SET c.enabled = ?2 WHERE c.id = ?1")
+	@Modifying
+	public void updateEnabledStatus(Integer id, boolean enabled);	
 }
