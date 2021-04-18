edc48775e089f85bc1f21b1bc00f75501e0dda42
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-19 00:23:00 +0300

    Task 203 : Create CategoryPageInfo

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/CategoryPageInfo.java
@@ -0,0 +1,20 @@
+package com.shopme.admin.util;
+
+public class CategoryPageInfo {
+
+	private int totalPages;
+	private long totalElements;
+
+	public int getTotalPages() {
+		return totalPages;
+	}
+	public void setTotalPages(int totalPages) {
+		this.totalPages = totalPages;
+	}
+	public long getTotalElements() {
+		return totalElements;
+	}
+	public void setTotalElements(long totalElements) {
+		this.totalElements = totalElements;
+	}
+}
