fe5c1b13021dc18fe59d0ffef548aaa441b9cb90
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-19 00:17:37 +0300

    Task 198 : Add listByPage and delete in ICategoryService

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/ICategoryService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/ICategoryService.java
@@ -3,11 +3,13 @@ package com.shopme.admin.service.impl;
 import java.util.List;
 
 import com.shopme.admin.error.CategoryNotFoundException;
+import com.shopme.admin.util.CategoryPageInfo;
 import com.shopme.common.entity.Category;
 
 public interface ICategoryService {
 
-	public List<Category> listAll(String sortDir);
+	public List<Category> listByPage(CategoryPageInfo pageInfo, int pageNum, String sortDir,
+			String keyword);
 	
 	public List<Category> listCategoriesUsedInForm();
 	
@@ -18,4 +20,6 @@ public interface ICategoryService {
 	public String checkUnique(Integer id, String name, String alias);
 	
 	public void updateCategoryEnabledStatus(Integer id, boolean enabled);
+	
+	public void delete(Integer id) throws CategoryNotFoundException;
 }
