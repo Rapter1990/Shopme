6beb1b57d5bf5643b8af0d7c7c6c5156473b2e70
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-20 00:13:31 +0300

    Task 217 : Define CategoryCsvExporter, CategoryExcelExporter, CategoryPdfExporter

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/CategoryCsvExporter.java
@@ -0,0 +1,39 @@
+package com.shopme.admin.util;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.supercsv.io.CsvBeanWriter;
+import org.supercsv.io.ICsvBeanWriter;
+import org.supercsv.prefs.CsvPreference;
+
+import com.shopme.common.entity.Category;
+
+public class CategoryCsvExporter extends AbstractExporter {
+	public void export(List<Category> listCategories, HttpServletResponse response) 
+			throws IOException {
+		super.setResponseHeader(response, "text/csv", ".csv", "categories_");
+
+		Writer writer = new OutputStreamWriter(response.getOutputStream(), "utf-8");
+		writer.write('\uFEFF');
+		
+		ICsvBeanWriter csvWriter = new CsvBeanWriter(writer, 
+				CsvPreference.STANDARD_PREFERENCE);
+
+		String[] csvHeader = {"Category ID", "Category Name"};
+		String[] fieldMapping = {"id", "name"};
+
+		csvWriter.writeHeader(csvHeader);
+
+		for (Category category : listCategories) {
+			category.setName(category.getName().replace("--", "  "));
+			csvWriter.write(category, fieldMapping);
+		}
+
+		csvWriter.close();
+	}
+}
---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/CategoryExcelExporter.java
@@ -0,0 +1,89 @@
+package com.shopme.admin.util;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import com.shopme.common.entity.Category;
+
+public class CategoryExcelExporter extends AbstractExporter{
+
+	private XSSFWorkbook workbook;
+	private XSSFSheet sheet;
+	
+
+	public CategoryExcelExporter() {
+		workbook = new XSSFWorkbook();
+	}
+
+	private void writeHeaderLine() {
+		sheet = workbook.createSheet("Category");
+		XSSFRow row = sheet.createRow(0);
+
+		XSSFCellStyle cellStyle = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setBold(true);
+		font.setFontHeight(16);
+		cellStyle.setFont(font);
+
+		createCell(row, 0, "Category Id", cellStyle);
+		createCell(row, 1, "Category Name", cellStyle);
+
+	}
+
+	private void createCell(XSSFRow row, int columnIndex, Object value, CellStyle style) {
+		XSSFCell cell = row.createCell(columnIndex);
+		sheet.autoSizeColumn(columnIndex);
+
+		if (value instanceof Integer) {
+			cell.setCellValue((Integer) value);
+		} else if (value instanceof Boolean) {
+			cell.setCellValue((Boolean) value);
+		} else {
+			cell.setCellValue((String) value);
+		}
+
+		cell.setCellStyle(style);		
+	}
+
+	public void export(List<Category> listCategories, HttpServletResponse response) throws IOException {
+		super.setResponseHeader(response, "application/octet-stream", ".xlsx", "categories_");
+
+		writeHeaderLine();
+		writeDataLines(listCategories);
+
+		ServletOutputStream outputStream = response.getOutputStream();
+		workbook.write(outputStream);
+		workbook.close();
+		outputStream.close();
+
+
+	}
+
+	private void writeDataLines(List<Category> listCategories) {
+		int rowIndex = 1;
+
+		XSSFCellStyle cellStyle = workbook.createCellStyle();
+		XSSFFont font = workbook.createFont();
+		font.setFontHeight(14);
+		cellStyle.setFont(font);
+
+		for (Category category : listCategories) {
+			XSSFRow row = sheet.createRow(rowIndex++);
+			int columnIndex = 0;
+
+			createCell(row, columnIndex++, category.getId(), cellStyle);
+			createCell(row, columnIndex++, category.getName(), cellStyle);
+		}
+	}
+}
