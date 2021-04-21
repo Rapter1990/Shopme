c36eeb0b30f516125b6c3a01a34dee929382bdf8
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-22 02:26:37 +0300

    Task 223 : Create BrandRepository

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.shopme.admin.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.shopme.common.entity.Brand;
+
+public interface BrandRepository extends PagingAndSortingRepository<Brand, Integer> {
+
+}
