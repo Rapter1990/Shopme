5bf3df2cc465a17cf00835a79d0489d13414fd28
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-23 23:24:04 +0300

    Task 227 : Create BrandRepositoryTests

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/BrandRepositoryTests.java
@@ -0,0 +1,100 @@
+package com.shopme.admin.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.admin.repository.BrandRepository;
+import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class BrandRepositoryTests {
+
+	@Autowired
+	private BrandRepository repo;
+	
+	@Test
+	public void testCreateBrand1() {
+		Category laptops = new Category(6);
+		Brand acer = new Brand("Acer");
+		acer.getCategories().add(laptops);
+
+		Brand savedBrand = repo.save(acer);
+
+		assertThat(savedBrand).isNotNull();
+		assertThat(savedBrand.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testCreateBrand2() {
+		Category cellphones = new Category(4);
+		Category tablets = new Category(7);
+
+		Brand apple = new Brand("Apple");
+		apple.getCategories().add(cellphones);
+		apple.getCategories().add(tablets);
+
+		Brand savedBrand = repo.save(apple);
+
+		assertThat(savedBrand).isNotNull();
+		assertThat(savedBrand.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testCreateBrand3() {
+		Brand samsung = new Brand("Samsung");
+
+		samsung.getCategories().add(new Category(29));	// category memory
+		samsung.getCategories().add(new Category(24));	// category internal hard drive
+
+		Brand savedBrand = repo.save(samsung);
+
+		assertThat(savedBrand).isNotNull();
+		assertThat(savedBrand.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testFindAll() {
+		Iterable<Brand> brands = repo.findAll();
+		brands.forEach(System.out::println);
+
+		assertThat(brands).isNotEmpty();
+	}
+
+	@Test
+	public void testGetById() {
+		Brand brand = repo.findById(1).get();
+
+		assertThat(brand.getName()).isEqualTo("Acer");
+	}
+
+	@Test
+	public void testUpdateName() {
+		String newName = "Samsung Electronics";
+		Brand samsung = repo.findById(3).get();
+		samsung.setName(newName);
+
+		Brand savedBrand = repo.save(samsung);
+		assertThat(savedBrand.getName()).isEqualTo(newName);
+	}
+
+	@Test
+	public void testDelete() {
+		Integer id = 2;
+		repo.deleteById(id);
+
+		Optional<Brand> result = repo.findById(id);
+
+		assertThat(result.isEmpty());
+	}
+}
