e623f6170a3e47d8199189e3577f357aed55d4c8
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-26 12:19:59 +0300

    Task 234 : Add CategoryService and Define newBrand in BrandController

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/BrandController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/BrandController.java
@@ -8,20 +8,43 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.shopme.admin.service.BrandService;
+import com.shopme.admin.service.CategoryService;
 import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
 
 @Controller
 public class BrandController {
 
+	private BrandService brandService;
+	
+	private CategoryService categoryService;
+	
 	@Autowired
-	private BrandService service;
+	public BrandController(BrandService brandService, CategoryService categoryService) {
+		super();
+		this.brandService = brandService;
+		this.categoryService = categoryService;
+	}
+
+
 
 	@GetMapping("/brands")
 	public String listAll(Model model) {
-		List<Brand> listBrands = service.listAll();
+		List<Brand> listBrands = brandService.listAll();
 		model.addAttribute("listBrands", listBrands);
 
 		return "brands/brands";
+	}
+	
+	@GetMapping("/brands/new")
+	public String newBrand(Model model) {
+		List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+
+		model.addAttribute("listCategories", listCategories);
+		model.addAttribute("brand", new Brand());
+		model.addAttribute("pageTitle", "Create New Brand");
+
+		return "brands/brand_form";		
 	}
 	
 }
