4e1c7211ab66856989dcb1d203d30f321cfd0973
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-04-29 00:39:04 +0300

    Task 242 : Define save,get,delete function in IBrandService

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/IBrandService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/IBrandService.java
@@ -2,10 +2,14 @@ package com.shopme.admin.service.impl;
 
 import java.util.List;
 
+import com.shopme.admin.error.BrandNotFoundException;
 import com.shopme.common.entity.Brand;
 
 public interface IBrandService {
 
 	public List<Brand> listAll();
+	public Brand save(Brand brand);
+	public Brand get(Integer id) throws BrandNotFoundException;
+	public void delete(Integer id) throws BrandNotFoundException;
 	
 }
