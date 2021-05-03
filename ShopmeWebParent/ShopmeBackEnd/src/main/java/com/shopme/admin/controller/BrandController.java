455d8c2123f75411a3c6da7ea7352de6ecdf70ee
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-03 11:51:39 +0300

    Task 265 : Define listFirstPage and listByPage methods and add loggers for each process in BrandController

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/BrandController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/BrandController.java
@@ -3,7 +3,11 @@ package com.shopme.admin.controller;
 import java.io.IOException;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -23,6 +27,8 @@ import com.shopme.common.entity.Category;
 
 @Controller
 public class BrandController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(BrandController.class);
 
 	private BrandService brandService;
 	
@@ -36,22 +42,26 @@ public class BrandController {
 	}
 
 
-
 	@GetMapping("/brands")
-	public String listAll(Model model) {
-		List<Brand> listBrands = brandService.listAll();
-		model.addAttribute("listBrands", listBrands);
-
-		return "brands/brands";
+	public String listFirstPage(Model model) {
+		
+		LOGGER.info("BrandController | listByPage is started");
+		
+		return listByPage(1, model, "name", "asc", null);
 	}
 	
 	@GetMapping("/brands/new")
 	public String newBrand(Model model) {
+		
+		LOGGER.info("BrandController | newBrand is started");
+		
 		List<Category> listCategories = categoryService.listCategoriesUsedInForm();
-
+		
 		model.addAttribute("listCategories", listCategories);
 		model.addAttribute("brand", new Brand());
 		model.addAttribute("pageTitle", "Create New Brand");
+		
+		LOGGER.info("CategoryController | newBrand | listCategories : " + listCategories.toString());
 
 		return "brands/brand_form";		
 	}
@@ -59,12 +69,24 @@ public class BrandController {
 	@PostMapping("/brands/save")
 	public String saveBrand(Brand brand, @RequestParam("fileImage") MultipartFile multipartFile,
 			RedirectAttributes ra) throws IOException {
+		
+		LOGGER.info("BrandController | saveBrand is started");
+		
+		LOGGER.info("BrandController | saveBrand | multipartFile.isEmpty() : " + multipartFile.isEmpty());
+		
+		
 		if (!multipartFile.isEmpty()) {
 			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			
+			LOGGER.info("BrandController | saveCategory | fileName : " + fileName);
+			
 			brand.setLogo(fileName);
 
 			Brand savedBrand = brandService.save(brand);
 			String uploadDir = "../brand-logos/" + savedBrand.getId();
+			
+			LOGGER.info("BrandController | saveBrand | savedBrand : " + savedBrand.toString());
+			LOGGER.info("BrandController | saveBrand | uploadDir : " + uploadDir);
 
 			FileUploadUtil.cleanDir(uploadDir);
 			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
@@ -80,9 +102,15 @@ public class BrandController {
 	@GetMapping("/brands/edit/{id}")
 	public String editBrand(@PathVariable(name = "id") Integer id, Model model,
 			RedirectAttributes ra) {
+		
+		LOGGER.info("BrandController | editBrand is started");
+		
 		try {
 			Brand brand = brandService.get(id);
 			List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+			
+			LOGGER.info("BrandController | editBrand | brand : " + brand.toString());
+			LOGGER.info("BrandController | editBrand | listCategories : " + listCategories.toString());
 
 			model.addAttribute("brand", brand);
 			model.addAttribute("listCategories", listCategories);
@@ -99,10 +127,24 @@ public class BrandController {
 	public String deleteBrand(@PathVariable(name = "id") Integer id, 
 			Model model,
 			RedirectAttributes redirectAttributes) {
+		
+		LOGGER.info("BrandController | deleteBrand is started");
+		
+		LOGGER.info("BrandController | deleteBrand | id : " + id);
+		
 		try {
 			brandService.delete(id);
+			
+			LOGGER.info("BrandController | deleteBrand | brand deleted");
+			
 			String brandDir = "../brand-logos/" + id;
+			
+			LOGGER.info("BrandController | deleteBrand | brandDir : " + brandDir);
+			
 			FileUploadUtil.removeDir(brandDir);
+			
+			LOGGER.info("BrandController | deleteBrand | FileUploadUtil.removeDir is over");
+			
 
 			redirectAttributes.addFlashAttribute("messageSuccess", 
 					"The brand ID " + id + " has been deleted successfully");
@@ -113,4 +155,34 @@ public class BrandController {
 		return "redirect:/brands";
 	}	
 	
+	@GetMapping("/brands/page/{pageNum}")
+	public String listByPage(
+			@PathVariable(name = "pageNum") int pageNum, Model model,
+			@Param("sortField") String sortField, @Param("sortDir") String sortDir,
+			@Param("keyword") String keyword
+			) {
+		Page<Brand> page = brandService.listByPage(pageNum, sortField, sortDir, keyword);
+		List<Brand> listBrands = page.getContent();
+
+		long startCount = (pageNum - 1) * BrandService.BRANDS_PER_PAGE + 1;
+		long endCount = startCount + BrandService.BRANDS_PER_PAGE - 1;
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+		model.addAttribute("keyword", keyword);		
+		model.addAttribute("listBrands", listBrands);
+
+		return "brands/brands";		
+	}
 }
