9f44b64fcc1cc4d6886d2f2dca36796be002618c
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-03 11:50:01 +0300

    Task 263 : Add listByPage method in IBrandService

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/IBrandService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/IBrandService.java
@@ -2,6 +2,8 @@ package com.shopme.admin.service.impl;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 import com.shopme.admin.error.BrandNotFoundException;
 import com.shopme.common.entity.Brand;
 
@@ -12,4 +14,5 @@ public interface IBrandService {
 	public Brand get(Integer id) throws BrandNotFoundException;
 	public void delete(Integer id) throws BrandNotFoundException;
 	public String checkUnique(Integer id, String name);
+	public Page<Brand> listByPage(int pageNum, String sortField, String sortDir, String keyword);
 }
