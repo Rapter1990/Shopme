81232106b84768994f00793be3114334f78f6afe
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-03 11:49:17 +0300

    Task 262 : Add findAll with search option in BrandRepository

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/BrandRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/BrandRepository.java
@@ -1,5 +1,8 @@
 package com.shopme.admin.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.shopme.common.entity.Brand;
@@ -9,5 +12,8 @@ public interface BrandRepository extends PagingAndSortingRepository<Brand, Integ
 	public Long countById(Integer id);
 	
 	public Brand findByName(String name);
+	
+	@Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
+	public Page<Brand> findAll(String keyword, Pageable pageable);
 	
 }
