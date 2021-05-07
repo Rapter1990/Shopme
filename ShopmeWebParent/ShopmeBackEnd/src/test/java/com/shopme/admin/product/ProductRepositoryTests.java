194245116fb94b97569ca93f3cd59caaac52c35f
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-07 11:40:56 +0300

    Task 280 : Define ProductRepositoryTests

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/product/ProductRepositoryTests.java
@@ -0,0 +1,98 @@
+package com.shopme.admin.product;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Date;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.admin.repository.ProductRepository;
+import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
+import com.shopme.common.entity.Product;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class ProductRepositoryTests {
+
+	@Autowired
+	private ProductRepository repo;
+
+	@Autowired
+	private TestEntityManager entityManager;
+	
+	@Test
+	public void testCreateProduct() {
+		Brand brand = entityManager.find(Brand.class, 37);
+		Category category = entityManager.find(Category.class, 5);
+
+		Product product = new Product();
+		product.setName("Acer Aspire Desktop");
+		product.setAlias("acer_aspire_desktop");
+		product.setShortDescription("Short description for Acer Aspire");
+		product.setFullDescription("Full description for Acer Aspire");
+
+		product.setBrand(brand);
+		product.setCategory(category);
+
+		product.setPrice(678);
+		product.setCost(600);
+		product.setEnabled(true);
+		product.setInStock(true);
+
+		product.setCreatedTime(new Date());
+		product.setUpdatedTime(new Date());
+
+		Product savedProduct = repo.save(product);
+
+		assertThat(savedProduct).isNotNull();
+		assertThat(savedProduct.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testListAllProducts() {
+		Iterable<Product> iterableProducts = repo.findAll();
+
+		iterableProducts.forEach(System.out::println);
+	}
+
+	@Test
+	public void testGetProduct() {
+		Integer id = 1;
+		Product product = repo.findById(id).get();
+		System.out.println(product);
+
+		assertThat(product).isNotNull();
+	}
+
+	@Test
+	public void testUpdateProduct() {
+		Integer id = 1;
+		Product product = repo.findById(id).get();
+		product.setPrice(499);
+
+		repo.save(product);
+
+		Product updatedProduct = entityManager.find(Product.class, id);
+
+		assertThat(updatedProduct.getPrice()).isEqualTo(499);
+	}
+
+	@Test
+	public void testDeleteProduct() {
+		Integer id = 1;
+		repo.deleteById(id);
+
+		Optional<Product> result = repo.findById(id);
+
+		assertThat(!result.isPresent());
+	}
+}
