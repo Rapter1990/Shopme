27eb669ca152fd638e647fd663c5275c88d7ee5c
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-09 16:02:44 +0300

    Task 283 : Create IProductService and define listAll

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/IProductService.java
@@ -0,0 +1,10 @@
+package com.shopme.admin.service.impl;
+
+import java.util.List;
+
+import com.shopme.common.entity.Product;
+
+public interface IProductService {
+
+	public List<Product> listAll();
+}
