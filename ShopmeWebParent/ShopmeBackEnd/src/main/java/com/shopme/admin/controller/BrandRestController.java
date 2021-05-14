31902675a6021e1be31abefa0722218f638d0f5d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-14 15:47:32 +0300

    Task 291 : Define listCategoriesByBrand in BrandRestController

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/BrandRestController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/BrandRestController.java
@@ -1,11 +1,22 @@
 package com.shopme.admin.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.shopme.admin.error.BrandNotFoundException;
+import com.shopme.admin.error.BrandNotFoundRestException;
 import com.shopme.admin.service.BrandService;
+import com.shopme.admin.util.CategoryDTO;
+import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
 
 @RestController
 public class BrandRestController {
@@ -16,5 +27,24 @@ public class BrandRestController {
 	@PostMapping("/brands/check_unique")
 	public String checkUnique(@Param("id") Integer id, @Param("name") String name) {
 		return service.checkUnique(id, name);
+	}
+	
+	@GetMapping("/brands/{id}/categories")
+	public List<CategoryDTO> listCategoriesByBrand(@PathVariable(name = "id") Integer brandId) throws BrandNotFoundRestException {
+		List<CategoryDTO> listCategories = new ArrayList<>(); 
+
+		try {
+			Brand brand = service.get(brandId);
+			Set<Category> categories = brand.getCategories();
+
+			for (Category category : categories) {
+				CategoryDTO dto = new CategoryDTO(category.getId(), category.getName());
+				listCategories.add(dto);
+			}
+
+			return listCategories;
+		} catch (BrandNotFoundException e) {
+			throw new BrandNotFoundRestException();
+		}
 	}
 }
