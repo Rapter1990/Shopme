8fa46424bf5db1b9d4220ee29a8f04f14b54676a
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-14 15:45:00 +0300

    Task 287 : Add findAll in BrandRepository

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/BrandRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/BrandRepository.java
@@ -1,5 +1,7 @@
 package com.shopme.admin.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
@@ -16,4 +18,7 @@ public interface BrandRepository extends PagingAndSortingRepository<Brand, Integ
 	@Query("SELECT b FROM Brand b WHERE b.name LIKE %?1%")
 	public Page<Brand> findAll(String keyword, Pageable pageable);
 	
+	// NEW -> new anonymous type with only the properties you need
+	@Query("SELECT NEW Brand(b.id, b.name) FROM Brand b ORDER BY b.name ASC")
+	public List<Brand> findAll();
 }
