516c30f18a7228cb8da871dd1a221c1886feeee2
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-14 15:46:54 +0300

    Task 290 : Create CategoryDTO

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/CategoryDTO.java
@@ -0,0 +1,31 @@
+package com.shopme.admin.util;
+
+public class CategoryDTO {
+	private Integer id;
+	private String name;
+
+	public CategoryDTO() {
+	}
+
+	public CategoryDTO(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
