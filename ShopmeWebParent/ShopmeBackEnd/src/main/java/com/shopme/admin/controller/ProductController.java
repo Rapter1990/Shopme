290cf10a376b67e52efe4fd14eca08c9f6e38d94
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-23 12:55:30 +0300

    Task 309 : Implement updateEnabledStatus into ProductController

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -81,4 +82,16 @@ public class ProductController {
 
 		return "redirect:/products";
 	}
+	
+	@GetMapping("/products/{id}/enabled/{status}")
+	public String updateCategoryEnabledStatus(@PathVariable("id") Integer id,
+			@PathVariable("status") boolean enabled, RedirectAttributes redirectAttributes) {
+		
+		productService.updateProductEnabledStatus(id, enabled);
+		String status = enabled ? "enabled" : "disabled";
+		String message = "The Product ID " + id + " has been " + status;
+		redirectAttributes.addFlashAttribute("message", message);
+
+		return "redirect:/products";
+	}	
 }
