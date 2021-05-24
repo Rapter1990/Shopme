82cecce6602b2f9de8a8d4e18acdfcfbe9b03a31
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-24 12:52:39 +0300

    Task 315 : Create ProductImage

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/ProductImage.java
@@ -0,0 +1,59 @@
+package com.shopme.common.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product_images")
+public class ProductImage {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@ManyToOne
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+	public ProductImage() {
+	}
+
+	public ProductImage(String name, Product product) {
+		this.name = name;
+		this.product = product;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+}
