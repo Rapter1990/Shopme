98454a478d8f97ad1bb79b059d173a7f7ec24ab0
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-24 12:54:19 +0300

    Task 317 : Define testSaveProductWithImages in ProductRepositoryTests

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/product/ProductRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/product/ProductRepositoryTests.java
@@ -95,4 +95,19 @@ public class ProductRepositoryTests {
 
 		assertThat(!result.isPresent());
 	}
+	
+	@Test
+	public void testSaveProductWithImages() {
+		Integer productId = 1;
+		Product product = repo.findById(productId).get();
+
+		product.setMainImage("main image.jpg");
+		product.addExtraImage("extra image 1.png");
+		product.addExtraImage("extra_image_2.png");
+		product.addExtraImage("extra-image3.png");
+
+		Product savedProduct = repo.save(product);
+
+		assertThat(savedProduct.getImages().size()).isEqualTo(3);
+	}
 }
