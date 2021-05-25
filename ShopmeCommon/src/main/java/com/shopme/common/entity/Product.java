c2af49955332631e55d713a901cc6e0e0bf71d0f
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-25 07:40:24 +0300

    Task 320 : Define getMainImagePath in Product

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -15,6 +15,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -84,6 +85,13 @@ public class Product implements Serializable{
 
 	public void addExtraImage(String imageName) {
 		this.images.add(new ProductImage(imageName, this));
+	}
+	
+	@Transient
+	public String getMainImagePath() {
+		if (id == null || mainImage == null) return "/images/image-thumbnail.png";
+
+		return "/product-images/" + this.id + "/" + this.mainImage;
 	}
 	
 	@Override
