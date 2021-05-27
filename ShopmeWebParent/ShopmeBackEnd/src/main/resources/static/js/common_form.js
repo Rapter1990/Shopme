d3715cc6471f3dd199a606add969c50d8ad8593d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-05-27 11:25:01 +0300

    Task 327 : Define checkFileSize method in common_form.js

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_form.js
@@ -7,18 +7,30 @@ $(document).ready(function() {
 	});
 
 	$("#fileImage").change(function() {
-		fileSize = this.files[0].size;
-
-		if (fileSize > MAX_FILE_SIZE) {
-			this.setCustomValidity("You must choose an image less than " + MAX_FILE_SIZE + " bytes!");
-			this.reportValidity();
-		} else {
-			this.setCustomValidity("");
-			showImageThumbnail(this);				
+		if (!checkFileSize(this)) {
+			return;
 		}
+
+		showImageThumbnail(this);
 
 	});
 });
+
+function checkFileSize(fileInput) {
+	fileSize = fileInput.files[0].size;
+
+	if (fileSize > MAX_FILE_SIZE) {
+		fileInput.setCustomValidity("You must choose an image less than " + MAX_FILE_SIZE + " bytes!");
+		fileInput.reportValidity();
+
+		return false;
+	} else {
+		fileInput.setCustomValidity("");
+
+		return true;
+	}	
+}
+
 
 function showImageThumbnail(fileInput) {
 	var file = fileInput.files[0];
