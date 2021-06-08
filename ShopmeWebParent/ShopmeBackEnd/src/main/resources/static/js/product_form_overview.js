eceaf2e6b539a27d04e728a8d31404cbd1670c40
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-08 23:58:16 +0300

    Task 341 : Define getCategoriesForNewForm to determine if editmode is false

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form_overview.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form_overview.js
@@ -11,9 +11,20 @@ $(document).ready(function() {
 		getCategories();
 	});	
 
-	getCategories();
+	getCategoriesForNewForm();
 
 });
+
+function getCategoriesForNewForm() {
+	catIdField = $("#categoryId");
+	editMode = false;
+
+	if (catIdField.length) {
+		editMode = true;
+	}
+
+	if (!editMode) getCategories();
+}
 
 function getCategories() {
 	brandId = dropdownBrands.val(); 
