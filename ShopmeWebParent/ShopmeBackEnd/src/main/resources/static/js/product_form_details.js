15df7fc75afdb4abbee63e0f5e7104b344fda1da
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-11 23:45:40 +0300

    Task 351 : Define hidden inputs as Product detail id in product_form_details.js and product_details.html

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form_details.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/product_form_details.js
@@ -14,6 +14,7 @@ function addNextDetailSection() {
 
 	htmlDetailSection = `
 		<div class="form-inline" id="divDetail${divDetailsCount}">
+		    <input type="hidden" name="detailIDs" value="0" />
 			<label class="m-3">Name:</label>
 			<input type="text" class="form-control w-25" name="detailNames" maxlength="255" />
 			<label class="m-3">Value:</label>
