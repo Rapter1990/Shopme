6093152b8ce4c16851a67014f19942aae69a35be
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-18 22:51:03 +0300

    Task 365 : Define listFirstPage and listByPage instead of listAll in ProductController

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
@@ -11,6 +11,8 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -47,17 +49,58 @@ public class ProductController {
 	}
 
 	@GetMapping("/products")
-	public String listAll(Model model) {
+	public String listFirstPage(Model model) {
 		
-		LOGGER.info("ProductController | listAll is started");
+		LOGGER.info("ProductController | listFirstPage is started");
 		
-		List<Product> listProducts = productService.listAll();
+		return listByPage(1, model, "name", "asc", null);
+	}
+	
+	
+	@GetMapping("/products/page/{pageNum}")
+	public String listByPage(
+			@PathVariable(name = "pageNum") int pageNum, Model model,
+			@Param("sortField") String sortField, @Param("sortDir") String sortDir,
+			@Param("keyword") String keyword
+			) {
 		
-		LOGGER.info("ProductController | listAll | listProducts size : " + listProducts.size());
+		LOGGER.info("ProductController | listByPage is started");
+		
+		Page<Product> page = productService.listByPage(pageNum, sortField, sortDir, keyword);
+		List<Product> listProducts = page.getContent();
+		
+		LOGGER.info("ProductController | listByPage | listProducts size : " + listProducts.size() );
 
+		long startCount = (pageNum - 1) * ProductService.PRODUCTS_PER_PAGE + 1;
+		long endCount = startCount + ProductService.PRODUCTS_PER_PAGE - 1;
+		
+		LOGGER.info("ProductController | listByPage | startCount : " + startCount );
+		LOGGER.info("ProductController | listByPage | endCount : " + endCount );
+		
+		LOGGER.info("ProductController | listByPage | page.getTotalElements() : " + page.getTotalElements() );
+		
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+		
+		LOGGER.info("ProductController | listByPage | endCount : " + endCount );
+
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
+		
+		LOGGER.info("ProductController | listByPage | reverseSortDir : " + reverseSortDir );
+
+		model.addAttribute("currentPage", pageNum);
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
+		model.addAttribute("keyword", keyword);		
 		model.addAttribute("listProducts", listProducts);
 
-		return "products/products";
+		return "products/products";	
 	}
 	
 	@GetMapping("/products/new")
