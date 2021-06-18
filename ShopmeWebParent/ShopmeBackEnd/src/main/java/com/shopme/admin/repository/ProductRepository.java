771479fcda6ee2540bab15d9949603ad7dccafe4
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-18 22:45:02 +0300

    Task 363 : Define findAll for Pagination Process in ProductRepository

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/ProductRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.shopme.admin.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -15,4 +17,12 @@ public interface ProductRepository extends PagingAndSortingRepository<Product, I
 	public void updateEnabledStatus(Integer id, boolean enabled);
 	
 	public Long countById(Integer id);
+	
+	
+	@Query("SELECT p FROM Product p WHERE p.name LIKE %?1% " 
+			+ "OR p.shortDescription LIKE %?1% "
+			+ "OR p.fullDescription LIKE %?1% "
+			+ "OR p.brand.name LIKE %?1% "
+			+ "OR p.category.name LIKE %?1%")
+	public Page<Product> findAll(String keyword, Pageable pageable);
 }
