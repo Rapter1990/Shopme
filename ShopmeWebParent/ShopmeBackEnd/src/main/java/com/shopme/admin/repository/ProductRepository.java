3c61cf88fa957a6fdd56b35e7d543fa37b78f33d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-22 12:52:20 +0300

    Task 373 : Define findAllInCategory and searchInCategory methods in ProductRepository

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/ProductRepository.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/ProductRepository.java
@@ -25,4 +25,19 @@ public interface ProductRepository extends PagingAndSortingRepository<Product, I
 			+ "OR p.brand.name LIKE %?1% "
 			+ "OR p.category.name LIKE %?1%")
 	public Page<Product> findAll(String keyword, Pageable pageable);
+	
+	@Query("SELECT p FROM Product p WHERE p.category.id = ?1 "
+			+ "OR p.category.allParentIDs LIKE %?2%")	
+	public Page<Product> findAllInCategory(Integer categoryId, String categoryIdMatch, 
+			Pageable pageable);
+
+	@Query("SELECT p FROM Product p WHERE (p.category.id = ?1 "
+			+ "OR p.category.allParentIDs LIKE %?2%) AND "
+			+ "(p.name LIKE %?3% " 
+			+ "OR p.shortDescription LIKE %?3% "
+			+ "OR p.fullDescription LIKE %?3% "
+			+ "OR p.brand.name LIKE %?3% "
+			+ "OR p.category.name LIKE %?3%)")			
+	public Page<Product> searchInCategory(Integer categoryId, String categoryIdMatch, 
+			String keyword, Pageable pageable);
 }
