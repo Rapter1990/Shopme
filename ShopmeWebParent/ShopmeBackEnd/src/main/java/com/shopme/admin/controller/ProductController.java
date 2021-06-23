ef798ed5642002cdf6ebe43bcea7b460ef6ef6a7
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-23 08:58:07 +0300

    Task 388 : Add Authontication feature and revise some codes in saveProduct method of ProductController

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/ProductController.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -24,6 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.error.ProductNotFoundException;
+import com.shopme.admin.security.ShopmeUserDetails;
 import com.shopme.admin.service.BrandService;
 import com.shopme.admin.service.CategoryService;
 import com.shopme.admin.service.ProductService;
@@ -143,13 +145,14 @@ public class ProductController {
 
 	@PostMapping("/products/save")
 	public String saveProduct(Product product, RedirectAttributes ra,
-			@RequestParam("fileImage") MultipartFile mainImageMultipart,
-			@RequestParam("extraImage") MultipartFile[] extraImageMultiparts,
+			@RequestParam(value = "fileImage", required = false) MultipartFile mainImageMultipart,			
+			@RequestParam(value = "extraImage", required = false) MultipartFile[] extraImageMultiparts,
 			@RequestParam(name = "detailIDs", required = false) String[] detailIDs,
 			@RequestParam(name = "detailNames", required = false) String[] detailNames,
 			@RequestParam(name = "detailValues", required = false) String[] detailValues,
 			@RequestParam(name = "imageIDs", required = false) String[] imageIDs,
-			@RequestParam(name = "imageNames", required = false) String[] imageNames
+			@RequestParam(name = "imageNames", required = false) String[] imageNames,
+			@AuthenticationPrincipal ShopmeUserDetails loggedUser
 			) throws IOException {
 		
 		LOGGER.info("ProductController | saveProduct is started");
@@ -157,6 +160,16 @@ public class ProductController {
 		LOGGER.info("ProductController | saveProduct | mainImageMultipart.isEmpty() : " + mainImageMultipart.isEmpty());
 		
 		LOGGER.info("ProductController | saveProduct | extraImageMultiparts size : " + extraImageMultiparts.length);
+		
+		LOGGER.info("ProductController | saveProduct | loggedUser.getUsername() : " + loggedUser.getUsername());
+		LOGGER.info("ProductController | saveProduct | loggedUser.getFullname() : " + loggedUser.getFullname());
+		LOGGER.info("ProductController | saveProduct | loggedUser.getAuthorities() : " + loggedUser.getAuthorities());
+		
+		if (loggedUser.hasRole("Salesperson")) {
+			productService.saveProductPrice(product);
+			ra.addFlashAttribute("messageSuccess", "The product has been saved successfully.");			
+			return "redirect:/products";			
+		}
 		
 		setMainImageName(mainImageMultipart, product);
 		
