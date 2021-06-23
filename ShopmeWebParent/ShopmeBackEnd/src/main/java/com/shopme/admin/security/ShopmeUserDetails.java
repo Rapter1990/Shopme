92d28aae0ecd5881757dcbd305c6c5a336426132
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-23 08:59:01 +0300

    Task 389 : Add serial version into ShopmeUserDetails

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
@@ -14,6 +14,9 @@ import com.shopme.common.entity.User;
 
 public class ShopmeUserDetails implements UserDetails {
 	
+	
+	private static final long serialVersionUID = 1L;
+	
 	private User user;
 	
 	
