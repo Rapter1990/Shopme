bc20dd90d47531df8edf9c5c62168c3c665955ab
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-26 18:33:19 +0300

    Task 400 : Define new locations of MainController and ShopmeFrontEndApplication

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/ShopmeFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.shopme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmeFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopmeFrontEndApplication.class, args);
+	}
+
+}
