389649d05c49dd6bcac189294a49a9d521677c2e
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-28 13:15:17 +0300

    Task 409 : Revise viewHomePage method to show all categories list without their subcategories in MainController

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/controller/MainController.java
@@ -0,0 +1,27 @@
+package com.shopme.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shopme.common.entity.Category;
+import com.shopme.service.CategoryService;
+
+@Controller
+public class MainController {
+	
+	@Autowired 
+	private CategoryService categoryService;
+	
+
+	@GetMapping("")
+	public String viewHomePage(Model model) {
+		List<Category> listCategories = categoryService.listNoChildrenCategories();
+		model.addAttribute("listCategories", listCategories);
+		
+		return "index";
+	}
+}
