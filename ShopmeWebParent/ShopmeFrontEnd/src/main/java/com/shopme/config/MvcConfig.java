ceca0fa9a2dec68532877f5e2696585c4b28beb8
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-28 13:17:21 +0300

    Task 410 : Define MvcConfig to detect all images in front end part

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/config/MvcConfig.java
@@ -0,0 +1,29 @@
+package com.shopme.config;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		exposeDirectory("../category-images", registry);
+		exposeDirectory("../brand-logos", registry);
+		exposeDirectory("../product-images", registry);
+	}
+
+	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
+		Path path = Paths.get(pathPattern);
+		String absolutePath = path.toFile().getAbsolutePath();
+
+		String logicalPath = pathPattern.replace("../", "") + "/**";
+
+		registry.addResourceHandler(logicalPath)
+			.addResourceLocations("file:/" + absolutePath + "/");		
+	}
+}
