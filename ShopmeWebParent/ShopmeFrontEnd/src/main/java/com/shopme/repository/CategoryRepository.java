5fa230e8ddca931596e242a1989fc78fb2ee73eb
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-28 12:39:58 +0300

    Task 406 : Create CategoryRepository

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/repository/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.shopme.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.shopme.common.entity.Category;
+
+public interface CategoryRepository extends CrudRepository<Category, Integer>{
+
+	@Query("SELECT c FROM Category c WHERE c.enabled = true ORDER BY c.name ASC")
+	public List<Category> findAllEnabled();
+}
