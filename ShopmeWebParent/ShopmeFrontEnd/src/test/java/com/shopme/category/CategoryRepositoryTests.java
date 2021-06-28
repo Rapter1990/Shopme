867b011e3089ad46fc26aeeeedbfd42c4a5393e3
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-28 12:45:50 +0300

    Task 407 : Create CategoryRepositoryTests and Implement testListEnabledCategories method

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/category/CategoryRepositoryTests.java
@@ -0,0 +1,28 @@
+package com.shopme.category;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.shopme.common.entity.Category;
+import com.shopme.repository.CategoryRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class CategoryRepositoryTests {
+
+	@Autowired 
+	private CategoryRepository repo;
+
+	@Test
+	public void testListEnabledCategories() {
+		List<Category> categories = repo.findAllEnabled();
+		categories.forEach(category -> {
+			System.out.println(category.getName() + " (" + category.isEnabled() + ")");
+		});
+	}
+}
