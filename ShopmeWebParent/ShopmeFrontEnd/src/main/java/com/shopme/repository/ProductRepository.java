58b01d535c21bec19276ccd6cd85f0092a7463e8
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-29 13:06:59 +0300

    Task 416 : Create ProductRepository and add listByCategory

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.shopme.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import com.shopme.common.entity.Product;
+
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
+
+	@Query("SELECT p FROM Product p WHERE p.enabled = true "
+			+ "AND (p.category.id = ?1 OR p.category.allParentIDs LIKE %?2%)"
+			+ " ORDER BY p.name ASC")
+	public Page<Product> listByCategory(Integer categoryId, String categoryIDMatch, Pageable pageable);
+}
