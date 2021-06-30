13547ac5d318f34df8762625c7fc0bf9ed54638f
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-06-30 12:06:45 +0300

    Task 429 : Revise getProduct method in IProductService and Implement it into ProductService

--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/service/ProductService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.shopme.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -7,11 +9,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.shopme.common.entity.Product;
+import com.shopme.common.error.ProductNotFoundException;
+import com.shopme.controller.ProductController;
 import com.shopme.repository.ProductRepository;
 import com.shopme.service.impl.IProductService;
 
 @Service
 public class ProductService implements IProductService{
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ProductService.class);
 
 	public static final int PRODUCTS_PER_PAGE = 10;
 
@@ -21,10 +27,24 @@ public class ProductService implements IProductService{
 	@Override
 	public Page<Product> listByCategory(int pageNum, Integer categoryId) {
 		// TODO Auto-generated method stub
+		
+		LOGGER.info("ProductService | listByCategory is called");
+		
 		String categoryIdMatch = "-" + String.valueOf(categoryId) + "-";
 		Pageable pageable = PageRequest.of(pageNum - 1, PRODUCTS_PER_PAGE);
+		
+		LOGGER.info("ProductService | listByCategory | categoryId : " + categoryId);
+		LOGGER.info("ProductService | listByCategory | categoryIdMatch : " + categoryIdMatch);
+		LOGGER.info("ProductService | listByCategory | pageable : " + pageable);
+		
 
 		return repo.listByCategory(categoryId, categoryIdMatch, pageable);
+	}
+
+	@Override
+	public Product getProduct(String alias) throws ProductNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/service/impl/IProductService.java
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/service/impl/IProductService.java
@@ -3,8 +3,11 @@ package com.shopme.service.impl;
 import org.springframework.data.domain.Page;
 
 import com.shopme.common.entity.Product;
+import com.shopme.common.error.ProductNotFoundException;
 
 public interface IProductService {
 
 	public Page<Product> listByCategory(int pageNum, Integer categoryId);
+	
+	public Product getProduct(String alias) throws ProductNotFoundException;
 }
