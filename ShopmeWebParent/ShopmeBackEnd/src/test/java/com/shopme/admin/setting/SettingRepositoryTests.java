7cdb010d39d1a4802077d1dcaac8c4e82ccbf715
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-06 00:49:30 +0300

    Task 447 : Create SettingRepositoryTests with adding testCreateGeneralSettings and testCreateCurrencySettings

---
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/setting/SettingRepositoryTests.java
@@ -0,0 +1,52 @@
+package com.shopme.admin.setting;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.admin.repository.SettingRepository;
+import com.shopme.common.entity.Setting;
+import com.shopme.common.entity.SettingCategory;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class SettingRepositoryTests {
+
+	@Autowired 
+	SettingRepository repo;
+
+	@Test
+	public void testCreateGeneralSettings() {
+		Setting siteName = new Setting("SITE_NAME", "Shopme", SettingCategory.GENERAL);
+		Setting siteLogo = new Setting("SITE_LOGO", "Shopme.png", SettingCategory.GENERAL);
+		Setting copyright = new Setting("COPYRIGHT", "Copyright (C) 2021 Shopme Ltd.", SettingCategory.GENERAL);
+
+		repo.saveAll(List.of(siteName, siteLogo, copyright));
+
+		Iterable<Setting> iterable = repo.findAll();
+
+		assertThat(iterable).size().isGreaterThan(0);
+	}
+
+	@Test
+	public void testCreateCurrencySettings() {
+		Setting currencyId = new Setting("CURRENCY_ID", "1", SettingCategory.CURRENCY);
+		Setting symbol = new Setting("CURRENCY_SYMBOL", "$", SettingCategory.CURRENCY);
+		Setting symbolPosition = new Setting("CURRENCY_SYMBOL_POSITION", "before", SettingCategory.CURRENCY);
+		Setting decimalPointType = new Setting("DECIMAL_POINT_TYPE", "POINT", SettingCategory.CURRENCY);
+		Setting decimalDigits = new Setting("DECIMAL_DIGITS", "2", SettingCategory.CURRENCY);
+		Setting thousandsPointType = new Setting("THOUSANDS_POINT_TYPE", "COMMA", SettingCategory.CURRENCY);
+
+		repo.saveAll(List.of(currencyId, symbol, symbolPosition, decimalPointType, 
+				decimalDigits, thousandsPointType));
+
+	}
+}
