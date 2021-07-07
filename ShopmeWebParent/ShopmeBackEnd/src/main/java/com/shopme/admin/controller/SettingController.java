ef255b38308f80795fffbefd3325fdf9d8ed63da
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-07 12:45:45 +0300

    Task 465 : Define saveGeneralSettings method in SettingController

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/SettingController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/SettingController.java
@@ -1,14 +1,24 @@
 package com.shopme.admin.controller;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.admin.helper.SettingHelper;
 import com.shopme.admin.repository.CurrencyRepository;
 import com.shopme.admin.service.SettingService;
+import com.shopme.admin.util.FileUploadUtil;
+import com.shopme.admin.util.GeneralSettingBag;
 import com.shopme.common.entity.Currency;
 import com.shopme.common.entity.Setting;
 
@@ -39,4 +49,21 @@ public class SettingController {
 
 		return "settings/settings";
 	}
+	
+	@PostMapping("/settings/save_general")
+	public String saveGeneralSettings(@RequestParam("fileImage") MultipartFile multipartFile,
+			HttpServletRequest request, RedirectAttributes ra) throws IOException {
+		GeneralSettingBag settingBag = service.getGeneralSettings();
+
+		SettingHelper.saveSiteLogo(multipartFile, settingBag);
+		SettingHelper.saveCurrencySymbol(request, settingBag,currencyRepo);
+
+		SettingHelper.updateSettingValuesFromForm(request, settingBag.list(),service);
+
+		ra.addFlashAttribute("messageSuccess", "General settings have been saved.");
+
+		return "redirect:/settings";
+	}
+
+	
 }
