abb7275567e1333b475a90c2042d864f4c1233de
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-07 12:35:27 +0300

    Task 462 : Create GeneralSettingBag extending from SettingBag Entity

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/GeneralSettingBag.java
@@ -0,0 +1,21 @@
+package com.shopme.admin.util;
+
+import java.util.List;
+
+import com.shopme.common.entity.Setting;
+import com.shopme.common.entity.SettingBag;
+
+public class GeneralSettingBag extends SettingBag {
+
+	public GeneralSettingBag(List<Setting> listSettings) {
+		super(listSettings);
+	}
+
+	public void updateCurrencySymbol(String value) {
+		super.update("CURRENCY_SYMBOL", value);
+	}
+
+	public void updateSiteLogo(String value) {
+		super.update("SITE_LOGO", value);
+	}
+}
