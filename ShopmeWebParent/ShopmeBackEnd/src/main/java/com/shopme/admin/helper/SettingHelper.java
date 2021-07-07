445b208e193a506fd77ba2b77dc7f366c4b0a827
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-07 12:45:12 +0300

    Task 464 : Define SettingHelper with adding saveSiteLogo,saveCurrencySymbol and updateSettingValuesFromForm method

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/helper/SettingHelper.java
@@ -0,0 +1,53 @@
+package com.shopme.admin.helper;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.shopme.admin.repository.CurrencyRepository;
+import com.shopme.admin.service.SettingService;
+import com.shopme.admin.util.FileUploadUtil;
+import com.shopme.admin.util.GeneralSettingBag;
+import com.shopme.common.entity.Currency;
+import com.shopme.common.entity.Setting;
+
+public class SettingHelper {
+
+	public static void saveSiteLogo(MultipartFile multipartFile, GeneralSettingBag settingBag) throws IOException {
+		if (!multipartFile.isEmpty()) {
+			String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+			String value = "/site-logo/" + fileName;
+			settingBag.updateSiteLogo(value);
+
+			String uploadDir = "../site-logo/";
+			FileUploadUtil.cleanDir(uploadDir);
+			FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+		}
+	}
+
+	public static void saveCurrencySymbol(HttpServletRequest request, GeneralSettingBag settingBag,CurrencyRepository currencyRepo) {
+		Integer currencyId = Integer.parseInt(request.getParameter("CURRENCY_ID"));
+		Optional<Currency> findByIdResult = currencyRepo.findById(currencyId);
+
+		if (findByIdResult.isPresent()) {
+			Currency currency = findByIdResult.get();
+			settingBag.updateCurrencySymbol(currency.getSymbol());
+		}
+	}
+
+	public static void updateSettingValuesFromForm(HttpServletRequest request, List<Setting> listSettings,SettingService service) {
+		for (Setting setting : listSettings) {
+			String value = request.getParameter(setting.getKey());
+			if (value != null) {
+				setting.setValue(value);
+			}
+		}
+
+		service.saveAll(listSettings);
+	}
+}
