97f2693283de339a9327a7f2017561bf9e1d4350
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-07 12:36:46 +0300

    Task 463 : Add getGeneralSettings and saveAll into ISettingService and Implement them into SettingService

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/SettingService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/SettingService.java
@@ -1,12 +1,15 @@
 package com.shopme.admin.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.shopme.admin.repository.SettingRepository;
 import com.shopme.admin.service.impl.ISettingService;
+import com.shopme.admin.util.GeneralSettingBag;
 import com.shopme.common.entity.Setting;
+import com.shopme.common.entity.SettingCategory;
 
 
 public class SettingService implements ISettingService{
@@ -20,4 +23,21 @@ public class SettingService implements ISettingService{
 		return (List<Setting>) repo.findAll();
 	}
 
+	@Override
+	public GeneralSettingBag getGeneralSettings() {
+		List<Setting> settings = new ArrayList<>();
+
+		List<Setting> generalSettings = repo.findByCategory(SettingCategory.GENERAL);
+		List<Setting> currencySettings = repo.findByCategory(SettingCategory.CURRENCY);
+
+		settings.addAll(generalSettings);
+		settings.addAll(currencySettings);
+
+		return new GeneralSettingBag(settings);
+	}
+
+	@Override
+	public void saveAll(Iterable<Setting> settings) {
+		repo.saveAll(settings);
+	}
 }
--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/ISettingService.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/service/impl/ISettingService.java
@@ -2,9 +2,14 @@ package com.shopme.admin.service.impl;
 
 import java.util.List;
 
+import com.shopme.admin.util.GeneralSettingBag;
 import com.shopme.common.entity.Setting;
 
 public interface ISettingService {
 
-	public List<Setting> listAllSettings() ;
+	public List<Setting> listAllSettings();
+	
+	public GeneralSettingBag getGeneralSettings();
+	
+	public void saveAll(Iterable<Setting> settings);
 }
