6e8faddb247d86d4099f8460d51cf32dffa5d6fb
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-07 12:15:24 +0300

    Task 459 : Create SettingBag Entity

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/SettingBag.java
@@ -0,0 +1,42 @@
+package com.shopme.common.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class SettingBag implements Serializable{
+
+	private List<Setting> listSettings;
+
+	public SettingBag(List<Setting> listSettings) {
+		this.listSettings = listSettings;
+	}
+
+	public Setting get(String key) {
+		int index = listSettings.indexOf(new Setting(key));
+		if (index >= 0) {
+			return listSettings.get(index);
+		}
+
+		return null;
+	}
+
+	public String getValue(String key) {
+		Setting setting = get(key);
+		if (setting != null) {
+			return setting.getValue();
+		}
+
+		return null;
+	}
+
+	public void update(String key, String value) {
+		Setting setting = get(key);
+		if (setting != null && value != null) {
+			setting.setValue(value);
+		}
+	}
+
+	public List<Setting> list() {
+		return listSettings;
+	}
+}
