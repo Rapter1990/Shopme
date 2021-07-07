713c8e75cf9782aea0ade732f85952288e06e7a9
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-07 12:50:48 +0300

    Task 467 : Add exposeDirectory for Site Logo to show its icon

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/config/MvcConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/config/MvcConfig.java
@@ -25,6 +25,9 @@ public class MvcConfig implements WebMvcConfigurer {
 		// Product
 		exposeDirectory("../product-images", registry);
 		
+		// Site Logo
+		exposeDirectory("../site-logo", registry);
+		
 	}
 
 	private void exposeDirectory(String pathPattern, ResourceHandlerRegistry registry) {
