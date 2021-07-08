749af940d6ce8a4d78fc14a8d2534191176f43af
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-08 12:46:09 +0300

    Task 472 : Define SettingRepository with adding testFindByTwoCategories method

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/setting/SettingRepositoryTests.java
@@ -0,0 +1,29 @@
+package com.shopme.setting;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Setting;
+import com.shopme.common.entity.SettingCategory;
+import com.shopme.repository.SettingRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class SettingRepositoryTests {
+
+	@Autowired 
+	SettingRepository repo;
+
+	@Test
+	public void testFindByTwoCategories() {
+		List<Setting> settings = repo.findByTwoCategories(SettingCategory.GENERAL, SettingCategory.CURRENCY);
+		settings.forEach(System.out::println);
+	}
+}
