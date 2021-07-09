77c2fb713d0a34a8ad951c40b6be6368919f1c6d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-09 11:31:38 +0300

    Task 481 : Create CountryRepository and StateRepository

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/repository/CountryRepository.java
@@ -0,0 +1,13 @@
+package com.shopme.admin.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.shopme.common.entity.Country;
+
+public interface CountryRepository extends CrudRepository<Country, Integer> {
+	
+	public List<Country> findAllByOrderByNameAsc();
+	
+}
