eb03d55c51c2e2230fd539ce15f03d9d40bb33cc
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-11 21:27:20 +0300

    Task 486 : Define CountryRestController with adding listAll, save (save or update) and delete methods

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/CountryRestController.java
@@ -0,0 +1,36 @@
+package com.shopme.admin.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shopme.admin.repository.CountryRepository;
+import com.shopme.common.entity.Country;
+
+@RestController
+public class CountryRestController {
+
+	@Autowired 
+	private CountryRepository repo;
+
+	@GetMapping("/countries/list")
+	public List<Country> listAll() {
+		return repo.findAllByOrderByNameAsc();
+	}
+
+	@PostMapping("/countries/save")
+	public String save(@RequestBody Country country) {
+		Country savedCountry = repo.save(country);
+		return String.valueOf(savedCountry.getId());
+	}
+
+	@GetMapping("/countries/delete/{id}")
+	public void delete(@PathVariable("id") Integer id) {
+		repo.deleteById(id);
+	}
+}
