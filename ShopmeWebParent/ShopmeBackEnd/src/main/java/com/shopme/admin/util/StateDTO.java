2be0e18b5fdee84f0451547e5e1df9ff44a793dc
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-14 09:48:04 +0300

    Task 491 : Create StateDTO

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/util/StateDTO.java
@@ -0,0 +1,31 @@
+package com.shopme.admin.util;
+
+public class StateDTO {
+	private Integer id;
+	private String name;
+
+	public StateDTO() {
+
+	}
+
+	public StateDTO(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}	
+}
