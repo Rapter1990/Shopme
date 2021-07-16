ab061502b5865ed26c893439e34d6959b50b345f
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-16 14:49:31 +0300

    Task 499 : Create Customer Entity

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Customer.java
@@ -0,0 +1,75 @@
+package com.shopme.common.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Table(name = "customers")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Customer implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(nullable = false, unique = true, length = 45)
+	private String email;
+
+	@Column(nullable = false, length = 64)
+	private String password;
+
+	@Column(name = "first_name", nullable = false, length = 45)
+	private String firstName;
+
+	@Column(name = "last_name", nullable = false, length = 45)
+	private String lastName;
+
+	@Column(name = "phone_number", nullable = false, length = 15)
+	private String phoneNumber;
+
+	@Column(nullable = false, length = 64)
+	private String addressLine1;
+
+	@Column(name = "address_line_2", length = 64)
+	private String addressLine2;
+
+	@Column(nullable = false, length = 45)
+	private String city;
+
+	@Column(nullable = false, length = 45)
+	private String state;
+
+	@Column(name = "postal_code", nullable = false, length = 10)
+	private String postalCode;	
+
+	@Column(name = "verification_code", length = 64)
+	private String verificationCode;	
+
+	private boolean enabled;
+
+	@Column(name = "created_time")
+	private Date createdTime;
+
+	@ManyToOne
+	@JoinColumn(name = "country_id")
+	private Country country;
+	
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", email=" + email + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+}
