46e237c40d5f51b0d7bcee0209a3fd05da5b2970
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-16 14:49:58 +0300

    Task 500 : Create CustomerRepository

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/repository/CustomerRepository.java
@@ -0,0 +1,20 @@
+package com.shopme.repository;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.shopme.common.entity.Customer;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+	@Query("SELECT c FROM Customer c WHERE c.email = ?1")
+	public Customer findByEmail(String email);
+
+	@Query("SELECT c FROM Customer c WHERE c.verificationCode = ?1")
+	public Customer findByVerificationCode(String code);
+
+	@Query("UPDATE Customer c SET c.enabled = true WHERE c.id = ?1")
+	@Modifying
+	public void enable(Integer id);	
+}
