a68e16eefa7b861c02b49d51bb6daf2ca398ee69
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-17 22:47:24 +0300

    Task 506 : Add Model in fragment to show warning and error message in fragments.hhtml and Create Register form in register_form.html supported by register.js

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/register.js
@@ -0,0 +1,75 @@
+var dropDownCountry;
+var dataListState;
+var fieldState;
+
+$(document).ready(function() {
+	dropDownCountry = $("#country");
+	dataListState = $("#listStates");
+	fieldState = $("#state");
+
+	dropDownCountry.on("change", function() {
+		loadStatesForCountry();
+		fieldState.val("").focus();
+	});
+});
+
+function loadStatesForCountry() {
+	selectedCountry = $("#country option:selected");
+	countryId = selectedCountry.val();
+	url = contextPath + "settings/list_states_by_country/" + countryId;
+
+	$.get(url, function(responseJSON) {
+		dataListState.empty();
+
+		$.each(responseJSON, function(index, state) {
+			$("<option>").val(state.name).text(state.name).appendTo(dataListState);
+		});
+
+	}).fail(function() {
+		alert('failed to connect to the server!');
+	});
+}
+
+function checkPasswordMatch(confirmPassword) {
+	if (confirmPassword.value != $("#password").val()) {
+		confirmPassword.setCustomValidity("Passwords do not match!");
+	} else {
+		confirmPassword.setCustomValidity("");
+	}
+}
+
+function checkEmailUnique(form) {
+	url = contextPath + "customers/check_unique_email";
+	customerEmail = $("#email").val();
+	csrfValue = $("input[name='_csrf']").val();
+
+	params = {email: customerEmail, _csrf: csrfValue};
+
+	$.post(url, params, function(response) {
+		if (response == "OK") {
+			form.submit();
+		} else if (response == "Duplicated") {
+			showWarningModal("There is another cusotmer having the email " + customerEmail);
+		} else {
+			showErrorModal("Unknown response from server");
+		}
+	}).fail(function() {
+		showErrorModal("Could not connect to the server");
+	});
+
+	return false;
+}	
+
+function showModalDialog(title, message) {
+	$("#modalTitle").text(title);
+	$("#modalBody").text(message);
+	$("#modalDialog").modal();
+}
+
+function showErrorModal(message) {
+	showModalDialog("Error", message);
+}
+
+function showWarningModal(message) {
+	showModalDialog("Warning", message);
+}	
