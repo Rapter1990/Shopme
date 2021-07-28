9e91201d2958e2c5d88150f098a2a16048a2640b
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-07-28 11:17:09 +0300

    Task 521 : Define EmailSettingBag

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/EmailSettingBag.java
@@ -0,0 +1,50 @@
+package com.shopme.common.entity;
+
+import java.util.List;
+
+public class EmailSettingBag extends SettingBag {
+
+	public EmailSettingBag(List<Setting> listSettings) {
+		super(listSettings);
+	}
+
+	public String getHost() {
+		return super.getValue("MAIL_HOST");
+	}
+
+	public int getPort() {
+		return Integer.parseInt(super.getValue("MAIL_PORT"));
+	}
+
+	public String getUsername() {
+		return super.getValue("MAIL_USERNAME");
+	}
+
+	public String getPassword() {
+		return super.getValue("MAIL_PASSWORD");
+	}
+
+	public String getSmtpAuth() {
+		return super.getValue("SMTP_AUTH");
+	}
+
+	public String getSmtpSecured() {
+		return super.getValue("SMTP_SECURED");
+	}
+
+	public String getFromAddress() {
+		return super.getValue("MAIL_FROM");
+	}
+
+	public String getSenderName() {
+		return super.getValue("MAIL_SENDER_NAME");
+	}
+
+	public String getCustomerVerifySubject() {
+		return super.getValue("CUSTOMER_VERIFY_SUBJECT");
+	}
+
+	public String getCustomerVerifyContent() {
+		return super.getValue("CUSTOMER_VERIFY_CONTENT");
+	}	
+}
