17bd6e5d62a8118a670a76676c50568e4ab7b60c
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-08-20 23:57:41 +0300

    Task 573 : Implement the process of formatting currency type in Shopping Cart

--- ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/shopping_cart.js
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/resources/static/js/shopping_cart.js
@@ -1,3 +1,6 @@
+decimalSeparator = decimalPointType == 'COMMA' ? ',' : '.';
+thousandsSeparator = thousandsPointType == 'COMMA' ? ',' : '.'; 
+
 $(document).ready(function() {
 	$(".linkMinus").on("click", function(evt) {
 		evt.preventDefault();
@@ -59,8 +62,7 @@ function updateQuantity(productId, quantity) {
 }
 
 function updateSubtotal(updatedSubtotal, productId) {
-	formattedSubtotal = $.number(updatedSubtotal, 2);
-	$("#subtotal" + productId).text(formattedSubtotal);
+	$("#subtotal" + productId).text(formatCurrency(updatedSubtotal));
 }
 
 function updateTotal() {
@@ -69,14 +71,14 @@ function updateTotal() {
 	
 	$(".subtotal").each(function(index, element) {
 		productCount++;
-		total += parseFloat(element.innerHTML.replaceAll(",", ""));
+		total += parseFloat(clearCurrencyFormat(element.innerHTML));
 	});
 
 	if (productCount < 1) {
 		showEmptyShoppingCart();
 	} else {
 		formattedTotal = $.number(total, 2);
-		$("#total").text(formattedTotal);		
+		$("#total").text(formatCurrency(total));	
 	}
 }
 
@@ -116,4 +118,13 @@ function updateCountNumbers() {
 	$(".divCount").each(function(index, element) {
 		element.innerHTML = "" + (index + 1);
 	}); 
+}
+
+function formatCurrency(amount) {
+	return $.number(amount, decimalDigits, decimalSeparator, thousandsSeparator);
+}
+
+function clearCurrencyFormat(numberString) {
+	result = numberString.replaceAll(thousandsSeparator, "");
+	return result.replaceAll(decimalSeparator, ".");
 }  
