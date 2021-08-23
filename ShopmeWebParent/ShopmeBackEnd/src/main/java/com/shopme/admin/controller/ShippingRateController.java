e5b529ef7295b89fd4011c69d129a213372d9494
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-08-24 00:58:28 +0300

    Task 577 : Add Comments into ShippingRateController and Fix the issue in product_overview.html

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/ShippingRateController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/ShippingRateController.java
@@ -2,6 +2,8 @@ package com.shopme.admin.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +22,8 @@ import com.shopme.common.exception.ShippingRateNotFoundException;
 
 @Controller
 public class ShippingRateController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ShippingRateController.class);
 
 	private String defaultRedirectURL = "redirect:/shipping_rates/page/1?sortField=country&sortDir=asc";
 
@@ -28,6 +32,9 @@ public class ShippingRateController {
 	
 	@GetMapping("/shipping_rates")
 	public String listFirstPage() {
+		
+		LOGGER.info("ShippingRateController | listFirstPage is called");
+		
 		return defaultRedirectURL;
 	}
 
@@ -35,13 +42,25 @@ public class ShippingRateController {
 	public String listByPage(@PagingAndSortingParam(listName = "shippingRates", 
 						moduleURL = "/shipping_rates") PagingAndSortingHelper helper,
 						@PathVariable(name = "pageNum") int pageNum) {
+		
+		LOGGER.info("ShippingRateController | listByPage is called");
+		
+		LOGGER.info("ShippingRateController | listByPage | helper : " + helper.toString());
+		LOGGER.info("ShippingRateController | listByPage | pageNum : " + pageNum);
+		
 		service.listByPage(pageNum, helper);
 		return "shipping_rates/shipping_rates";
 	}	
 
 	@GetMapping("/shipping_rates/new")
 	public String newRate(Model model) {
+		
+		
+		LOGGER.info("ShippingRateController | newRate is called");
+			
 		List<Country> listCountries = service.listAllCountries();
+		
+		LOGGER.info("ShippingRateController | newRate | listCountries : " + listCountries.toString());
 
 		model.addAttribute("rate", new ShippingRate());
 		model.addAttribute("listCountries", listCountries);
@@ -52,10 +71,19 @@ public class ShippingRateController {
 
 	@PostMapping("/shipping_rates/save")
 	public String saveRate(ShippingRate rate, RedirectAttributes ra) {
+		
+		LOGGER.info("ShippingRateController | saveRate is called");
+		
 		try {
 			service.save(rate);
+			
+			LOGGER.info("ShippingRateController | saveRate | message : " + "The shipping rate has been saved successfully.");
+			
 			ra.addFlashAttribute("message", "The shipping rate has been saved successfully.");
 		} catch (ShippingRateAlreadyExistsException ex) {
+			
+			LOGGER.info("ShippingRateController | saveRate | message : " + ex.getMessage());
+			
 			ra.addFlashAttribute("message", ex.getMessage());
 		}
 		return defaultRedirectURL;
@@ -64,9 +92,18 @@ public class ShippingRateController {
 	@GetMapping("/shipping_rates/edit/{id}")
 	public String editRate(@PathVariable(name = "id") Integer id,
 			Model model, RedirectAttributes ra) {
+		
+		LOGGER.info("ShippingRateController | editRate is called");
+		
+		
 		try {
 			ShippingRate rate = service.get(id);
+			
+			LOGGER.info("ShippingRateController | editRate | rate : " + rate.toString());
+			
 			List<Country> listCountries = service.listAllCountries();
+			
+			LOGGER.info("ShippingRateController | editRate | listCountries : " + listCountries.toString());
 
 			model.addAttribute("listCountries", listCountries);			
 			model.addAttribute("rate", rate);
@@ -74,6 +111,9 @@ public class ShippingRateController {
 
 			return "shipping_rates/shipping_rate_form";
 		} catch (ShippingRateNotFoundException ex) {
+			
+			LOGGER.info("ShippingRateController | saveRate | message : " + ex.getMessage());
+			
 			ra.addFlashAttribute("message", ex.getMessage());
 			return defaultRedirectURL;
 		}
@@ -83,10 +123,19 @@ public class ShippingRateController {
 	public String updateCODSupport(@PathVariable(name = "id") Integer id,
 			@PathVariable(name = "supported") Boolean supported,
 			Model model, RedirectAttributes ra) {
+		
+		LOGGER.info("ShippingRateController | updateCODSupport is called");
+		
 		try {
 			service.updateCODSupport(id, supported);
+			
+			LOGGER.info("ShippingRateController | updateCODSupport | message : " + "COD support for shipping rate ID " + id + " has been updated.");
+			
 			ra.addFlashAttribute("message", "COD support for shipping rate ID " + id + " has been updated.");
 		} catch (ShippingRateNotFoundException ex) {
+			
+			LOGGER.info("ShippingRateController | updateCODSupport | message : " + ex.getMessage());
+			
 			ra.addFlashAttribute("message", ex.getMessage());
 		}
 		return defaultRedirectURL;
@@ -95,10 +144,19 @@ public class ShippingRateController {
 	@GetMapping("/shipping_rates/delete/{id}")
 	public String deleteRate(@PathVariable(name = "id") Integer id,
 			Model model, RedirectAttributes ra) {
+		
+		LOGGER.info("ShippingRateController | deleteRate is called");
+		
 		try {
 			service.delete(id);
+			
+			LOGGER.info("ShippingRateController | deleteRate | message : " + "The shipping rate ID " + id + " has been deleted.");
+			
 			ra.addFlashAttribute("message", "The shipping rate ID " + id + " has been deleted.");
 		} catch (ShippingRateNotFoundException ex) {
+			
+			LOGGER.info("ShippingRateController | deleteRate | message : " + ex.getMessage());
+			
 			ra.addFlashAttribute("message", ex.getMessage());
 		}
 		return defaultRedirectURL;
