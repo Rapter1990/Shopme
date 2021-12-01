65e169bc3f713d11373d839c5f49f1a7d4806088
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-12-01 23:40:04 +0300

    Task 635 : Show Customer, Product and Review Detail Modal in Review List of ShopmeBackEnd

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/ReviewController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/ReviewController.java
@@ -7,10 +7,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.paging.PagingAndSortingHelper;
 import com.shopme.admin.paging.PagingAndSortingParam;
 import com.shopme.admin.service.ReviewService;
+import com.shopme.common.entity.Review;
+import com.shopme.common.exception.ReviewNotFoundException;
 
 @Controller
 public class ReviewController {
@@ -40,5 +43,29 @@ public class ReviewController {
 		service.listByPage(pageNum, helper);
 
 		return "reviews/reviews";
+	}
+	
+	@GetMapping("/reviews/detail/{id}")
+	public String viewReview(@PathVariable("id") Integer id, Model model, RedirectAttributes ra) {
+		
+		LOGGER.info("ReviewController | viewReview is started");
+		
+		LOGGER.info("ReviewController | viewReview | id : " + id);
+		
+		try {
+			Review review = service.get(id);
+			
+			LOGGER.info("ReviewController | viewReview | review : " + review.toString());
+			
+			model.addAttribute("review", review);
+
+			return "reviews/review_detail_modal";
+		} catch (ReviewNotFoundException ex) {
+			
+			LOGGER.info("ReviewController | viewReview | messageError : " + ex.getMessage());
+			
+			ra.addFlashAttribute("messageError", ex.getMessage());
+			return defaultRedirectURL;		
+		}
 	}
 }
