0e81db49801b751c3bd2bea716bd0e9af1c55c7a
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-12-03 00:30:38 +0300

    Task 636 : Implement the process of updating and deleting Review in Review List of ShopmeBackEnd

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/ReviewController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/controller/ReviewController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.shopme.admin.paging.PagingAndSortingHelper;
@@ -67,5 +68,68 @@ public class ReviewController {
 			ra.addFlashAttribute("messageError", ex.getMessage());
 			return defaultRedirectURL;		
 		}
+	}
+	
+	@GetMapping("/reviews/edit/{id}")
+	public String editReview(@PathVariable("id") Integer id, Model model, RedirectAttributes ra) {
+		
+		LOGGER.info("ReviewController | editReview is started");
+		
+		LOGGER.info("ReviewController | editReview | id : " + id);
+		
+		
+		try {
+			Review review = service.get(id);
+			
+			LOGGER.info("ReviewController | editReview | review : " + review.toString());
+			LOGGER.info("ReviewController | editReview | pageTitle : " + (String.format("Edit Review (ID: %d)", id)));
+
+			model.addAttribute("review", review);
+			model.addAttribute("pageTitle", String.format("Edit Review (ID: %d)", id));
+
+			return "reviews/review_form";
+		} catch (ReviewNotFoundException ex) {
+			
+			LOGGER.info("ReviewController | viewReview | messageError : " + ex.getMessage());
+			
+			ra.addFlashAttribute("messageError", ex.getMessage());
+			return defaultRedirectURL;		
+		}
+	}	
+
+	@PostMapping("/reviews/save")
+	public String saveReview(Review reviewInForm, RedirectAttributes ra) {
+		
+		LOGGER.info("ReviewController | saveReview is started");
+		
+		service.save(reviewInForm);		
+		
+		LOGGER.info("ReviewController | viewReview | messageSuccess : " + "The review ID " + reviewInForm.getId() + " has been updated successfully.");
+		
+		ra.addFlashAttribute("messageSuccess", "The review ID " + reviewInForm.getId() + " has been updated successfully.");
+		return defaultRedirectURL;		
+	}
+
+	@GetMapping("/reviews/delete/{id}")
+	public String deleteReview(@PathVariable("id") Integer id, RedirectAttributes ra) {
+		
+		LOGGER.info("ReviewController | deleteReview is started");
+		
+		LOGGER.info("ReviewController | deleteReview | id : " + id);
+		
+		try {
+			service.delete(id);
+			
+			LOGGER.info("ReviewController | viewReview | messageSuccess : " + "The review ID " + id + " has been deleted.");
+			
+			ra.addFlashAttribute("messageSuccess", "The review ID " + id + " has been deleted.");
+		} catch (ReviewNotFoundException ex) {
+			
+			LOGGER.info("ReviewController | viewReview | messageError : " + ex.getMessage());
+			
+			ra.addFlashAttribute("messageError", ex.getMessage());
+		}
+
+		return defaultRedirectURL;		
 	}
 }
