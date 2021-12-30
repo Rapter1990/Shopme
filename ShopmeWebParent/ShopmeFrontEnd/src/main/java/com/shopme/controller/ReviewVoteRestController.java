f13d3243918043159b7841d36c70aea7f142378c
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-12-30 13:13:25 +0300

    Task 658 : Create voteReview method in ReviewVoteRestController and Create testVoteNotLogin in ReviewVoteRestControllerTests

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/controller/ReviewVoteRestController.java
@@ -0,0 +1,63 @@
+package com.shopme.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shopme.common.entity.Customer;
+import com.shopme.common.entity.VoteResultDTO;
+import com.shopme.common.entity.VoteType;
+import com.shopme.common.exception.CustomerNotFoundException;
+import com.shopme.service.ReviewVoteService;
+import com.shopme.util.AuthenticationControllerHelperUtil;
+
+@RestController
+public class ReviewVoteRestController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ReviewVoteRestController.class);
+	
+	private ReviewVoteService service;
+	
+	private AuthenticationControllerHelperUtil helper;
+	
+	@Autowired
+	public ReviewVoteRestController(ReviewVoteService service, AuthenticationControllerHelperUtil helper) {
+		super();
+		this.service = service;
+		this.helper = helper;
+	}
+
+
+	@PostMapping("/vote_review/{id}/{type}")
+	public VoteResultDTO voteReview(@PathVariable(name = "id") Integer reviewId,
+			@PathVariable(name = "type") String type,
+			HttpServletRequest request) {
+		
+		LOGGER.info("ReviewVoteRestController | voteReview is called");
+		
+		LOGGER.info("ReviewVoteRestController | voteReview | reviewId : " + reviewId);
+		LOGGER.info("ReviewVoteRestController | voteReview | type : " + type);
+
+		Customer customer;
+		
+		try {
+			customer = helper.getAuthenticatedCustomer(request);
+		} catch (CustomerNotFoundException e) {
+			// TODO Auto-generated catch block
+			LOGGER.info("ReviewVoteRestController | voteReview | CustomerNotFoundException");
+			
+			return VoteResultDTO.fail("You must login to vote the review.");
+		}
+
+		VoteType voteType = VoteType.valueOf(type.toUpperCase());
+		
+		LOGGER.info("ReviewVoteRestController | voteReview | voteType : " + voteType);
+		
+		return service.doVote(reviewId, customer, voteType);
+	}
+}
---
+++ ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/review/ReviewVoteRestControllerTests.java
@@ -0,0 +1,59 @@
+package com.shopme.review;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.shopme.common.entity.Review;
+import com.shopme.common.entity.VoteResultDTO;
+import com.shopme.repository.ReviewRepository;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ReviewVoteRestControllerTests {
+
+	private ReviewRepository reviewRepo;
+	
+	private MockMvc mockMvc;
+	
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	public ReviewVoteRestControllerTests(ReviewRepository reviewRepo, MockMvc mockMvc, ObjectMapper objectMapper) {
+		super();
+		this.reviewRepo = reviewRepo;
+		this.mockMvc = mockMvc;
+		this.objectMapper = objectMapper;
+	}
+	
+	@Test
+	public void testVoteNotLogin() throws Exception {
+		String requestURL = "/vote_review/1/up";
+
+		MvcResult mvcResult = mockMvc.perform(post(requestURL).with(csrf()))
+			.andExpect(status().isOk())
+			.andDo(print())
+			.andReturn();
+
+		String json = mvcResult.getResponse().getContentAsString();
+		VoteResultDTO voteResult = objectMapper.readValue(json, VoteResultDTO.class);
+
+		assertFalse(voteResult.isSuccessful());
+		assertThat(voteResult.getMessage()).contains("You must login");
+	}
+
+}
