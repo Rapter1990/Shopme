7f741dd2b994f9ce897e43ea72837a4a9b9812f3
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-04-05 23:22:50 +0300

    Task 678 : Move some repetitive codes which defined in listing pages of ShopmeBackEnd into common.js

--- ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_list.js
+++ ShopmeWebParent/ShopmeBackEnd/src/main/resources/static/js/common_list.js
@@ -1,3 +1,11 @@
+$(document).ready(function() {
+	$(".link-delete").on("click", function(e) {
+		e.preventDefault();
+		showDeleteConfirmModal($(this), entityName);
+	});
+});
+
+
 function clearFilter() {
 	window.location = moduleURL;	
 }
@@ -9,4 +17,16 @@ function showDeleteConfirmModal(link, entityName) {
 	$("#confirmText").text("Are you sure you want to delete this "
 							 + entityName + " ID " + entityId + "?");
 	$("#confirmModal").modal();	
+}
+
+function handleDetailLinkClick(cssClass, modalId) {
+	$(cssClass).on("click", function(e) {
+		e.preventDefault();
+		linkDetailURL = $(this).attr("href");
+		$(modalId).modal("show").find(".modal-content").load(linkDetailURL);
+	});		
+}
+
+function handleDefaultDetailLinkClick() {
+	handleDetailLinkClick(".link-detail", "#detailModal");	
 }
