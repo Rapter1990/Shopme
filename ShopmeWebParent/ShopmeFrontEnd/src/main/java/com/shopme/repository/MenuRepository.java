773f8d09c3af37b4ba182d69ebedbf6614405c65
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-07-08 18:56:43 +0300

    Task 712 : Menu Module : Show all menu items by header and footer in MenuRepositoryTest of ShopmeFrontEnd

---
+++ ShopmeWebParent/ShopmeFrontEnd/src/main/java/com/shopme/repository/MenuRepository.java
@@ -0,0 +1,14 @@
+package com.shopme.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.shopme.common.entity.menu.Menu;
+import com.shopme.common.entity.menu.MenuType;
+
+public interface MenuRepository extends JpaRepository<Menu, Integer> {
+
+	public List<Menu> findByTypeAndEnabledOrderByPositionAsc(MenuType type, boolean enabled);
+
+}
---
+++ ShopmeWebParent/ShopmeFrontEnd/src/test/java/com/shopme/menu/MenuRepositoryTests.java
@@ -0,0 +1,40 @@
+package com.shopme.menu;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.shopme.common.entity.menu.Menu;
+import com.shopme.common.entity.menu.MenuType;
+import com.shopme.repository.MenuRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class MenuRepositoryTests {
+
+	@Autowired 
+	private MenuRepository repo;
+
+	@Test
+	public void testListHeaderMenuItems() {
+		List<Menu> listHeaderMenuItems = repo.findByTypeAndEnabledOrderByPositionAsc(MenuType.HEADER, true);
+		assertThat(listHeaderMenuItems).isNotEmpty();
+
+		listHeaderMenuItems.forEach(System.out::println);
+	}
+
+	@Test
+	public void testListFooterMenuItems() {
+		List<Menu> listFooterMenuItems = repo.findByTypeAndEnabledOrderByPositionAsc(MenuType.FOOTER, true);
+		assertThat(listFooterMenuItems).isNotEmpty();
+
+		listFooterMenuItems.forEach(System.out::println);
+	}
+	
+}
